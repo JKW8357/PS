1ce9f2e958a82b78397d86d29d1fa6d320cad9a8
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-19 00:33:51 +0900

    Update 줄어들지 않아.cc

--- 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.cc
+++ 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.cc
@@ -26,3 +26,20 @@ int main() {
 
 	return 0;
 }
+
+/*
+<accumulate() 사용 시 주의사항>
+
+accumulate 함수는 초기값의 타입을 기준으로 연산하므로,
+자료형에 따라 정확히 초기값의 타입을 명시해
+암묵적 형변환으로 인한 오버플로 또는 데이터 손실을 방지해야 한다.
+
+int: 0
+long long: 0LL
+unsigned long long
+float: 0.0f
+double: 0.0
+char: '\0'
+bool: false;
+pointer: nullptr
+*/

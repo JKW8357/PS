39538a29cb3064fb6301ddc47b149669c65e1827
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-19 16:35:45 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[26];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string str;
+	cin >> str;
+
+	int size = str.size();
+	for (int i = 0; i < size; i++)
+		arr[str[i] - 'A']++;
+
+	int oddcnt = 0;
+	char tmp = '\0';
+	for (int i = 0; i < 26; i++) {
+		if (arr[i] % 2 != 0) {
+			oddcnt++;
+			tmp = i + 'A';
+		}
+	}
+
+	string ans;
+	if (oddcnt == 0) {
+		for (int i = 0; i < 26; i++)
+			for (int j = 1; j <= arr[i] / 2; j++)
+				ans += i + 'A';
+
+		for (int i = 25; i >= 0; i--)
+			for (int j = 1; j <= arr[i] / 2; j++)
+				ans += i + 'A';
+	}
+
+	else if (oddcnt == 1) {
+		for (int i = 0; i < 26; i++)
+			for (int j = 1; j <= arr[i] / 2; j++)
+				ans += i + 'A';
+
+		ans += tmp;
+
+		for (int i = 25; i >= 0; i--)
+			for (int j = 1; j <= arr[i] / 2; j++)
+				ans += i + 'A';
+	}
+
+	else if (oddcnt > 1) ans = "I'm Sorry Hansoo";
+
+	cout << ans << '\n';
+	return 0;
+}

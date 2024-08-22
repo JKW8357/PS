2e811f3329b0dd78c9c1c17b4d874920466a5802
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-22 15:25:30 +0900

    [Silver I] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.cc
+++ 백준/Silver/2527. 직사각형/직사각형.cc
@@ -10,7 +10,7 @@ int main() {
 		cin >> xa1 >> ya1 >> xa2 >> ya2 >> xb1 >> yb1 >> xb2 >> yb2;
 		if (xa2 < xb1 || xb2 < xa1 || ya2 < yb1 || yb2 < ya1) cout << 'd' << '\n';
 		else if ((xa1 == xb2 || xa2 == xb1) && (ya1 == yb2 || ya2 == yb1)) cout << 'c' << '\n';
-		else if ((xa1 == xb2) || (xa2 == xb1) || (ya1 == yb2) || (ya2 == yb1)) cout << 'b' << '\n';
+		else if (xa1 == xb2 || xa2 == xb1 || ya1 == yb2 || ya2 == yb1) cout << 'b' << '\n';
 		else cout << 'a' << '\n';
 	}
 	

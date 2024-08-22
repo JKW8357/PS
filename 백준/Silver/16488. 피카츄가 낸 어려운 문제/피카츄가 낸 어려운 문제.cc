bd3ea024f8d6f21e948a3a9ffd38197c1894be1e
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-23 03:08:13 +0900

    [Silver IV] Title: 피카츄가 낸 어려운 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16488. 피카츄가 낸 어려운 문제/피카츄가 낸 어려운 문제.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long n, k;
+	cin >> n >> k;
+	cout << n * n * k;
+
+	return 0;
+}
+
+// 스튜어트 정리

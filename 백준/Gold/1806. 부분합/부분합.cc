1809558cf1bd16ef2a25b49f9b8081f17b72ba94
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-26 01:41:21 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[100005];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, s;
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	
+	int ans = INT_MAX;
+	int st = 0, en = 0;
+	int sum = 0;
+	while (st < n) {
+		while (en < n && sum < s)
+			sum += arr[en++];
+		if (en == n && sum < s) break;
+		ans = min(ans, en - st);
+		sum -= arr[st++];
+	}
+	
+	cout << (ans != INT_MAX ? ans : 0) << '\n';
+	return 0;
+}

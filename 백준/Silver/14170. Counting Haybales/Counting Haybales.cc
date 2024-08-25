20892e994c105e759fcc6946f2fa7bc50e43cb7f
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-25 15:08:56 +0900

    [Silver III] Title: Counting Haybales, Time: 64 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/14170. Counting Haybales/Counting Haybales.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, q;
+	cin >> n >> q;
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+
+	while (q--) {
+		int a, b;
+		cin >> a >> b;
+		int l = lower_bound(v.begin(), v.end(), a) - v.begin();
+		int r = upper_bound(v.begin(), v.end(), b) - v.begin();
+		cout << r - l << '\n';
+	}
+
+	return 0;
+}

28088b29200d84059373950f0066c219ef847c73
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-25 18:06:55 +0900

    [Gold IV] Title: 합이 0, Time: 1836 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long arr[10005];
+vector<int> vec;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -10,16 +10,19 @@ int main() {
 	int n;
 	cin >> n;
 
-	for (int i = 0; i < n; i++) cin >> arr[i];
-	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		vec.push_back(num);
+	}
+	sort(vec.begin(), vec.end());
 
 	long long cnt = 0;
 	for (int i = 0; i < n - 2; i++) {
 		for (int j = i + 1; j < n - 1; j++) {
-			auto left = lower_bound(arr + j + 1, arr + n, -(arr[i] + arr[j])) - arr;
-			auto right = upper_bound(arr + j + 1, arr + n, -(arr[i] + arr[j])) - arr;
-			if (left != right)
-				cnt += right - left;
+			auto left = lower_bound(vec.begin() + j + 1, vec.end(), -(vec[i] + vec[j])) - vec.begin();
+			auto right = upper_bound(vec.begin() + j + 1, vec.end(), -(vec[i] + vec[j])) - vec.begin();
+			cnt += right - left;
 		}
 	}
 

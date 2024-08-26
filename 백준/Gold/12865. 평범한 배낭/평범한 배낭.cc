d96abc31cbf4a1c65333a60a243e6b3e3f4dbb79
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-27 01:58:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 int w[105];
 int v[105];
-int dp[105][100005];
+int dp[100005];
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -13,13 +13,10 @@ int main() {
 	cin >> n >> k;
 	for (int i = 1; i <= n; i++) cin >> w[i] >> v[i];
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (j - w[i] >= 0) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-			else dp[i][j] = dp[i - 1][j];
-		}
-	}
+	for (int i = 1; i <= n; i++)
+		for (int j = k; j >= 1; j--)
+			if (j - w[i] >= 0) dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
 
-	cout << dp[n][k] << '\n';
+	cout << dp[k] << '\n';
 	return 0;
 }

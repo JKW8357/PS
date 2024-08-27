2aa3ea9ab4bf7154ca6354480e7941d4ece2db8c
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-28 02:00:20 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string a, p;
+	cin >> a >> p;
+
+	cout << p << '\n';
+	return 0;
+}

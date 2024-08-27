021003c7994cdd22fbbb75f7c671c3f43eefdbea
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-28 02:46:29 +0900

    [Bronze I] Title: 원의 분할, Time: 0 ms, Memory: 2020 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/16478. 원의 분할/원의 분할.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	double a, b, c;
+	cin >> a >> b >> c;
+	cout << fixed;
+	cout.precision(6);
+	cout << a * c / b << '\n';
+	
+	return 0;
+}

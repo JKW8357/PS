634f168c04fdb73bb7f995d77158ee1c2a7dc77a
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-28 20:32:30 +0900

    [Silver II] Title: N번째 큰 수, Time: 260 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<>> pq;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int num;
+			cin >> num;
+			if (pq.size() < n) pq.push(num);
+			else if (num > pq.top()) {
+				pq.pop();
+				pq.push(num);
+			}
+		}
+	}
+
+	cout << pq.top() << '\n';
+	return 0;
+}

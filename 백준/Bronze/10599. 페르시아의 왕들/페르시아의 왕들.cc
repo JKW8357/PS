de75157e37cd31953f7009e5021f5e394e54e401
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-29 01:48:08 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	while (true) {
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		if (a == 0 && b == 0 && c == 0 && d == 0) break;
+		cout << c - b << ' ' << d - a << '\n';
+	}
+
+	return 0;
+}

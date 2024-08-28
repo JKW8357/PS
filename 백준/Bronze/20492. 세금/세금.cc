d8146111d2bb27d37ba7e9fed47bab3175febe5b
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-29 01:13:02 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long a;
+	cin >> a;
+	cout << a * 78 / 100 << ' ' << a * 956 / 1000;
+	return 0;
+}

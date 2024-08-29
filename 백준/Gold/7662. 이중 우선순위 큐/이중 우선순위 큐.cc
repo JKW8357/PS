14796221491b5850f19b141e684ce8b1f356ef39
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-29 13:47:56 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1232 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -20,7 +20,7 @@ int main() {
 				int cmd2;
 				cin >> cmd2;
 				if (ms.empty()) continue;
-				else if (cmd2 == 1) ms.erase(prev(ms.end()));
+				if (cmd2 == 1) ms.erase(prev(ms.end()));
 				else ms.erase(ms.begin());
 			}
 
@@ -41,6 +41,8 @@ int main() {
 /*
 이진 검색 트리 set을 사용하면 삽입, 삭제, 탐색 모두 O(logN)에 처리 가능
 중복되는 원소까지 고려해야 하는 경우 set이 아닌 multiset을 사용해야 함에 유의
+또한 ms.erase(ms.begin());으로 코드를 작성한다면 최솟값이 여러 개일 때 그 중 1개만 삭제하지만
+ms.erase(*ms.begin());과 같이 작성한다면 최솟값이 여러 개일 때 모든 최솟값을 지우게 됨에도 유의해야 함
 
 또한 임의 접근 가능 stl(array, vector, deque 등)은 반복자 간의 산술 연산이 가능하지만(it + n, it - n 등)
 그 외의 stl(list, forward_list, stack, queue, priority_queue, unordered_(multi)set/map, (multi)set/map 등)은

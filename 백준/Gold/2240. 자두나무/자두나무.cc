5594e3b17e90dce75c227664ec9eac20c410ad7a
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-29 19:12:24 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int plum[1005];
-int dp[1005][35][3];
+int dp[1005][35];
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -13,21 +13,20 @@ int main() {
 	for (int i = 1; i <= t; i++) cin >> plum[i];
 
 	for (int i = 1; i <= t; i++) {
-		if (plum[i] == 1) dp[i][0][1] = dp[i - 1][0][1] + 1;
+		if (plum[i] == 1) dp[i][0] = dp[i - 1][0] + 1;
 		for (int j = 1; j <= w; j++) {
 			if (i < j) break;
-			if (plum[i] == 1) {
-				dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][2]) + 1;
-				dp[i][j][2] = max(dp[i - 1][j][2], dp[i - 1][j - 1][1]);
-			}
-
-			else {
-				dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][2]);
-				dp[i][j][2] = max(dp[i - 1][j][2], dp[i - 1][j - 1][1]) + 1;
-			}
+			dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + ((j + plum[i]) % 2 != 0 ? 1 : 0);
 		}
 	}
 
-	cout << max(dp[t][w][1], dp[t][w][2]) << '\n';
+	cout << *max_element(dp[t], dp[t] + w + 1) << '\n';
 	return 0;
 }
+
+/*
+자두가 움직인 횟수가 홀수일 경우 2번 나무, 짝수일 경우 1번 나무에 가게 된다.
+dp[i][j]일 때 자두가 1번 나무에 있을 때와 2번 나무에 있을 때 각각의 자두 개수의 최댓깂을 구한다.
+그리고 '모든 j(이동 횟수)'에 대해 자두 개수의 최댓값을 구한다.
+(dp[t][w]가 자두 개수의 최댓값이라는 보장은 없다.)
+*/

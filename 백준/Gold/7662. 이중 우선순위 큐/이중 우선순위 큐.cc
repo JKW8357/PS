9b1df85240531e92e7337455af8c3229aebc18d5
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-29 13:43:03 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1272 ms, Memory: 49016 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	while (tc--) {
+		int k;
+		cin >> k;
+		multiset<int> ms;
+
+		while (k--) {
+			char cmd1;
+			cin >> cmd1;
+			if (cmd1 == 'D') {
+				int cmd2;
+				cin >> cmd2;
+				if (ms.empty()) continue;
+				else if (cmd2 == 1) ms.erase(prev(ms.end()));
+				else ms.erase(ms.begin());
+			}
+
+			else {
+				int num;
+				cin >> num;
+				ms.insert(num);
+			}
+		}
+
+		if (ms.empty()) cout << "EMPTY" << '\n';
+		else cout << *prev(ms.end()) << ' ' << * ms.begin() << '\n';
+	}
+
+	return 0;
+}
+
+/*
+이진 검색 트리 set을 사용하면 삽입, 삭제, 탐색 모두 O(logN)에 처리 가능
+중복되는 원소까지 고려해야 하는 경우 set이 아닌 multiset을 사용해야 함에 유의
+
+또한 임의 접근 가능 stl(array, vector, deque 등)은 반복자 간의 산술 연산이 가능하지만(it + n, it - n 등)
+그 외의 stl(list, forward_list, stack, queue, priority_queue, unordered_(multi)set/map, (multi)set/map 등)은
+<iterator> 헤더의 advance, prev, next 등 반복자 조작 함수들을 이용해 반복자를 조작해야 함
+*/

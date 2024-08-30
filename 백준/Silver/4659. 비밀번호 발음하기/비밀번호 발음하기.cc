42a590e50b51c8836083598bbaceb104ede0a88a
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-30 17:33:09 +0900

    Update 비밀번호 발음하기.cc

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,39 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool function1(string pwd, string vowels) {
+	for (int i = 0; i < vowels.size(); i++)
+		if (pwd.find(vowels[i]) != string::npos) return true;
+	return false;
+}
+
+bool function2(string pwd, string vowels) {
+	int v = 0, c = 0;
+	for (int i = 0; i < pwd.size(); i++) {
+		if (vowels.find(pwd[i]) != string::npos) { c = 0; v++; }
+		else { v = 0; c++; }
+		if (v == 3 || c == 3) return false;
+	}
+	return true;
+}
+
+bool function3(string pwd) {
+	for (int i = 0; i < pwd.size() - 1; i++)
+		if (pwd[i] == pwd[i + 1] && pwd[i] != 'e' && pwd[i] != 'o') return false;
+	return true;
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	string vowels = "aeiou";
+
 	string pwd;
 	while (true) {
 		cin >> pwd;
 		if (pwd == "end") break;
 
-		bool check1 = false;
-		for (int i = 0; i < vowels.size(); i++)
-			if (pwd.find(vowels[i]) != string::npos) { check1 = true; break; }
-
-		bool check2 = true;
-		int v = 0, c = 0;
-		if (check1) {
-			for (int i = 0; i < pwd.size(); i++) {
-				if (vowels.find(pwd[i]) != string::npos) { c = 0; v++; }
-				else { v = 0; c++; }
-				if (v == 3 || c == 3) { check2 = false; break; }
-			}
-		}
-
-		bool check3 = true;
-		if (check1 && check2) {
-			for (int i = 0; i < pwd.size() - 1; i++) {
-				if (pwd[i] == pwd[i + 1]) {
-					if (pwd[i] == 'e' || pwd[i] == 'o') continue;
-					else { check3 = false; break; }
-				}
-			}
-		}
+		bool check1 = function1(pwd, vowels);
+		bool check2 = function2(pwd, vowels);
+		bool check3 = function3(pwd);
 
 		if (check1 && check2 && check3) cout << "<" << pwd << "> is acceptable.\n";
 		else cout << "<" << pwd << "> is not acceptable.\n";

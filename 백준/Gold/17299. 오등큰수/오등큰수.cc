7c6b31e12491f51bd9b32ce45d8474841c97339f
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-30 13:17:48 +0900

    [Gold III] Title: 오등큰수, Time: 212 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.cc
+++ 백준/Gold/17299. 오등큰수/오등큰수.cc
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
 
-int ngf[1000005];
+int arr[1000005];
 int cnt[1000005];
-pair<int, int> arr[1000005];
-stack<pair<int, int>> st;
+int ngf[1000005];
+stack<int> st;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -15,17 +13,13 @@ int main() {
 	int n;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		cin >> arr[i].X;
-		cnt[arr[i].X]++;
-	}
-
-	for (int i = 1; i <= n; i++) {
-		arr[i].Y = cnt[arr[i].X];
+		cin >> arr[i];
+		cnt[arr[i]]++;
 	}
 
 	for (int i = n; i >= 1; i--) {
-		while (!st.empty() && st.top().Y <= arr[i].Y) st.pop();
-		ngf[i] = (!st.empty() ? st.top().X : -1);
+		while (!st.empty() && cnt[st.top()] <= cnt[arr[i]]) st.pop();
+		ngf[i] = (!st.empty() ? st.top() : -1);
 		st.push(arr[i]);
 	}
 

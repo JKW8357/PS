1348c7c31c42ede950f080bfc21dcd8998460634
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-08-31 19:21:42 +0900

    [Silver II] Title: 싸이버개강총회, Time: 44 ms, Memory: 12808 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.cc
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.cc
@@ -3,30 +3,19 @@ using namespace std;
 
 unordered_map<string, bool> um;
 
-int calctime(string time) {
-	int h = stoi(time.substr(0, 2));
-	int s = stoi(time.substr(3, 2));
-	return 60 * h + s;
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	string s, e, q;
-	cin >> s >> e >> q;
-
-	int start = calctime(s);
-	int end = calctime(e);
-	int stream = calctime(q);
+	string start, end, stream;
+	cin >> start >> end >> stream;
 
 	int cnt = 0;
 	string time, name;
 
 	while (cin >> time >> name) {
-		int t = calctime(time);
-		if (t <= start) um[name] = true;
-		if (t >= end && t <= stream && um[name]) {
+		if (time <= start) um[name] = true;
+		if (time >= end && time <= stream && um[name]) {
 			um.erase(name);
 			cnt++;
 		}

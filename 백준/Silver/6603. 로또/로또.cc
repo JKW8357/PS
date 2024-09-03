6059fbe3cb0e533d4508d66cdbcfd52995e0fc6d
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-03 12:31:09 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int k;
+int arr[15];
+bool visited[15];
+vector<int> vec;
+
+void solve(int idx, int depth) {
+	if (depth == 6) {
+		for (int i : vec) cout << i << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = idx; i < k; i++) {
+		vec.push_back(arr[i]);
+		visited[i] = true;
+		solve(i + 1, depth + 1);
+		vec.pop_back();
+		visited[i] = false;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	while (true) {
+		cin >> k;
+		if (k == 0) break;
+		for (int i = 0; i < k; i++) cin >> arr[i];
+		solve(0, 0);
+		cout << '\n';
+	}
+
+	return 0;
+}

c7fcb7cf9a2b8d99803e3d7b99242770976239f5
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-03 10:13:03 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> vec;
+bool visited[10];
+
+void solve(int depth) {
+	if (depth == m) {
+		for (int i = 0; i < m; i++) cout << vec[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!visited[i]) {
+			vec.push_back(i);
+			visited[i] = true;
+			solve(depth + 1);
+			vec.pop_back();
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+	solve(0);
+
+	return 0;
+}

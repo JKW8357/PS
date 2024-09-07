40ab8fca80fa10753f18dd2f20404af2b0d3e17b
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-08 02:04:40 +0900

    Update 카잉 달력.cc

--- 백준/Silver/6064. 카잉 달력/카잉 달력.cc
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -3,9 +3,8 @@ using namespace std;
 
 int solve(int m, int n, int x, int y) {
 	int LCM = lcm(m, n);
-	for (int i = x; i <= LCM; i += m) {
-		if (i % n == (n == y ? 0 : y)) return i;
-	}
+	for (int i = x; i <= LCM; i += m)
+		if (i % n == (n == y ? 0 : y)) return i; // n == y일 때의 예외 처리
 	return -1;
 }
 
@@ -22,3 +21,10 @@ int main(void) {
 		cout << solve(m, n, x, y) << '\n';
 	}
 }
+
+/*
+연립 합동 방정식:
+i ≡ x (mod m)
+i ≡ y (mod n)
+x부터 시작해 m씩 더해가며 i가 y를 만족하는지 확인
+*/

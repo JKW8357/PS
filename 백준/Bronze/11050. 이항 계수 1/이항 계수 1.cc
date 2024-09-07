a0b25fecbdfff931ebc1a43c9a562b5a21e2df6a
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-07 18:47:35 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -1,23 +1,17 @@
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    int N{ 0 }, K{ 0 }, numerator{ 0 }, denominator{ 0 };
-    cin >> N >> K;
-    numerator = denominator = N;
+	int n, k;
+	cin >> n >> k;
 
-    for (int i = 0; i < K; ++i) {
-        numerator *= (N - i);
-        denominator *= (i + 1);
-    }
-
-    cout << numerator / denominator;
-
-    return 0;
+	int ans = 1, cnt = k;
+	while (cnt--) ans *= n--;
+	while (k) ans /= k--;
+	
+	cout << ans << '\n';
+	return 0;
 }

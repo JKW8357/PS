a24aa4fa98938b4105ec93263bf669368f37dbdb
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-08 00:35:03 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 12 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isprime(int n) {
-	if (n == 1) return false;
-	for (int i = 2; i * i <= n; i++)
-		if (n % i == 0) return false;
-	return true;
+vector<bool> state(2000001, true);
+void sieve() {
+	state[1] = false;
+	for (int i = 2; i * i <= 2000000; i++) {
+		if (!state[i]) continue;
+		for (int j = i * i; j <= 2000000; j += i)
+			state[j] = false;
+	}
+}
+
+bool ispalindrome(int n) {
+	string num = to_string(n);
+	string rev_num = num;
+	reverse(rev_num.begin(), rev_num.end());
+	return num == rev_num;
 }
 
 int main() {
@@ -14,17 +24,19 @@ int main() {
 
 	int n;
 	cin >> n;
-	
-	while(true) {
-		if (isprime(n)) {
-			string num = to_string(n);
-			string rev_num = to_string(n);
-			reverse(rev_num.begin(), rev_num.end());
-			if (num == rev_num) {
-				cout << num;
-				return 0;
-			}
+
+	sieve();
+	for (int i = n; i <= 2000000; i++) {
+		if (state[i] && ispalindrome(i)) {
+			cout << i << '\n';
+			return 0;
 		}
-		n++;
 	}
 }
+
+/*
+참고:
+1000000 이상의 가장 작은 팰린드롬 소수는 1003001
+따라서 탐색 범위는 최소 1003001 이상으로 설정해야 하며,
+state 배열의 크기는 이 범위를 포함하도록 충분히 크게 설정해야 함
+*/

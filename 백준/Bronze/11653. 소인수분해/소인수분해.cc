e19763a91732a3b305540342196bbbd8cc31df95
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-07 16:54:43 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,26 +1,18 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	unsigned int N{ 0 };
-	cin >> N;
-
-	while (N != 1) {
-		for (int i = 2; i <= N;) {
-			if (N % i == 0) {
-				cout << i <<'\n';
-				N /= i;
-			}
-			else {
-				++i;
-			}
+	int n;
+	cin >> n;
+	for (int i = 2; i * i <= n; i++) {
+		while (n % i == 0) {
+			cout << i << '\n';
+			n /= i;
 		}
 	}
-
+	if (n != 1) cout << n;
 	return 0;
 }

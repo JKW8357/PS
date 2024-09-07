e473ef7d4ef3b002935994263a4897a1b0c25192
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-07 18:35:19 +0900

    [Bronze II] Title: 만취한 상범, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6359. 만취한 상범/만취한 상범.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	
+	while (tc--) {
+		int n;
+		cin >> n;
+		cout << (int)sqrt(n) << '\n';
+	}
+
+	return 0;
+}

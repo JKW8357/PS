b54b2607d1c61b9d895f3ce7de66f92a297624b4
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-13 16:35:05 +0900

    [Silver III] Title: 잘 알려진 수열 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24516. 잘 알려진 수열 구하기/잘 알려진 수열 구하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 1; i <= 2 * n - 1; i += 2) cout << i << ' ';
+
+	return 0;
+}

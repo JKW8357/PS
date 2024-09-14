cf5696662b7b4ff2bed7fcdf21fb722b2c3db9ea
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-14 18:49:21 +0900

    [Gold IV] Title: 스도쿠, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -5,24 +5,17 @@ using namespace std;
 
 bool solved = false;
 int arr[10][10];
+bool isused1[10][10]; // isused1[row][num]: 행에서 num이 쓰였는지 확인
+bool isused2[10][10]; // isused2[col][num]: 열에서 num이 쓰였는지 확인
+bool isused3[10][10]; // isused3[box][num]: 3 * 3 박스에서 num이 쓰였는지 확인
 vector<pair<int, int>> blanks;
 
 bool check(int row, int col, int num) {
-	for (int i = 0; i < 9; i++)
-		if (arr[i][col] == num && i != row) return false;
-
-	for (int j = 0; j < 9; j++)
-		if (arr[row][j] == num && j != col) return false;
-
-	for (int i = 0; i < 9; i++)
-		for (int j = 0; j < 9; j++)
-			if (arr[i][j] == num && (i / 3 == row / 3) && (j / 3 == col / 3) && !(i == row && j == col)) return false;
-
-	return true;
+	return !isused1[row][num] && !isused2[col][num] && !isused3[row / 3 * 3 + col / 3][num];
 }
 
-void solve(int n, int depth) {
-	if (depth == n) {
+void solve(int depth) {
+	if (depth == blanks.size()) {
 		if (!solved) {
 			for (int i = 0; i < 9; i++) {
 				for (int j = 0; j < 9; j++)
@@ -34,13 +27,22 @@ void solve(int n, int depth) {
 		return;
 	}
 
-	for (int i = 1; i <= 9; i++) {
+	int row = blanks[depth].X;
+	int col = blanks[depth].Y;
+	for (int num = 1; num <= 9; num++) {
 		if (solved) return;
-		arr[blanks[depth].X][blanks[depth].Y] = i;
-		if (check(blanks[depth].X, blanks[depth].Y, i)) solve(n, depth + 1);
+		if (check(row, col, num)) {
+			arr[row][col] = num;
+			isused1[row][num] = true;
+			isused2[col][num] = true;
+			isused3[row / 3 * 3 + col / 3][num] = true;
+			solve(depth + 1);
+			isused1[row][num] = false;
+			isused2[col][num] = false;
+			isused3[row / 3 * 3 + col / 3][num] = false;
+			arr[row][col] = 0;
+		}
 	}
-
-	arr[blanks[depth].X][blanks[depth].Y] = 0;
 }
 
 int main() {
@@ -51,9 +53,24 @@ int main() {
 		for (int j = 0; j < 9; j++) {
 			cin >> arr[i][j];
 			if (arr[i][j] == 0) blanks.push_back({ i, j });
+			else {
+				isused1[i][arr[i][j]] = true;
+				isused2[j][arr[i][j]] = true;
+				isused3[i / 3 * 3 + j / 3][arr[i][j]] = true;
+			}
 		}
 	}
 
-	solve(blanks.size(), 0);
+	solve(0);
 	return 0;
 }
+
+/*
+isused1, isused2, isused3 배열을 통해
+행, 열, 3x3 박스에서 특정 숫자의 사용 여부를 미리 기록하고,
+이를 이용해 중복된 검사를 피함으로써 탐색 범위를 줄인다.
+
+각 칸에 숫자를 넣기 전에 이미 사용된 숫자는 즉시 배제하는 효율적인 가지치기를 통해
+불필요한 재귀 호출을 방지하고, 더 빠르게 정답을 찾을 수 있다.
+(N-Queen 문제의 코드 참고)
+*/

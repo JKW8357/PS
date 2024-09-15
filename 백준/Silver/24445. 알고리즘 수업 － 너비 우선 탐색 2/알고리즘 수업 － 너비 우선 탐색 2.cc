7215d72fe51a9134bcdc16b91881aa6b4faa647e
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-16 00:03:28 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 76 ms, Memory: 8840 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> graph[100005];
+int vis[100005];
+int order = 0;
+
+void bfs(int st) {
+	queue<int> q;
+	q.push(st);
+	vis[st] = ++order;
+	
+	while (!q.empty()) {
+		int cur = q.front(); q.pop();
+		for (int nxt : graph[cur]) {
+			if (vis[nxt] == 0) {
+				q.push(nxt);
+				vis[nxt] = ++order;
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m, r;
+	cin >> n >> m >> r;
+	while (m--) {
+		int u, v;
+		cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	for (int i = 1; i <= n; i++) sort(graph[i].begin(), graph[i].end(), greater<>());
+	bfs(r);
+	for (int i = 1; i <= n; i++) cout << vis[i] << '\n';
+	return 0;
+}

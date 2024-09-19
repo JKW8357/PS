0000d8e6bc2b14ababdcb14c0483b0475d69e59e
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-20 02:14:57 +0900

    [Gold III] Title: 텀 프로젝트, Time: 440 ms, Memory: 4240 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -3,22 +3,21 @@ using namespace std;
 
 int cnt;
 int choose[100005];
-bool vis[100005]; // 방문 여부 기록
-bool finished[100005]; // dfs 완료 여부 기록
+int state[100005];  // 상태 배열 (0: 방문하지 않음, 1: 방문함, 2: 탐색 완료)
 
 void dfs(int cur) {
-	vis[cur] = true;
+	state[cur] = 1;
 	int nxt = choose[cur];
 
-	if (!vis[nxt]) dfs(nxt); // 방문하지 않은 node에 대해 dfs
+	if (state[nxt] == 0) dfs(nxt); // 방문하지 않은 node에 대해 dfs
 	
 	// 재방문한 node에 대해 cycle 탐지
-	else if (!finished[nxt]) {
+	else if (state[nxt] == 1) {
 		for (int i = nxt; i != cur; i = choose[i]) cnt++; // cycle에 포함된 학생들 count
 		cnt++; // cur도 cycle에 포함
 	}
 
-	finished[cur] = true; // cur 탐색 완료
+	state[cur] = 2; // cur 탐색 완료
 }
 
 int main() {
@@ -35,11 +34,10 @@ int main() {
 		cnt = 0;
 		for (int i = 1; i <= n; i++) {
 			cin >> choose[i];
-			vis[i] = false;
-			finished[i] = false;
+			state[i] = 0;
 		}
 
-		for (int i = 1; i <= n; i++) dfs(i);
+		for (int i = 1; i <= n; i++) if (state[i] == 0) dfs(i); // 방문하지 않은 node에서만 dfs
 		cout << n - cnt << '\n';
 	}
 

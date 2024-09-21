10f09efb68b069f3ddadd93eb4611aeb80c023aa
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-21 16:34:39 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 484 ms, Memory: 48292 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+string board[1005];
+int dis[1005][1005][11];
+
+int bfs(int n, int m, int k) {
+	queue<tuple<int, int, int>> q;
+	q.push({ 0, 0, 0 });
+	dis[0][0][0] = 1;
+	
+	while (!q.empty()) {
+		auto cur = q.front(); q.pop();
+		int curX, curY, broken;
+		tie(curX, curY, broken) = cur;
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = curX + dx[dir];
+			int ny = curY + dy[dir];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+			if (board[nx][ny] == '0' && dis[nx][ny][broken] == -1) {
+				q.push({ nx, ny, broken });
+				dis[nx][ny][broken] = dis[curX][curY][broken] + 1;
+			}
+
+			else if (board[nx][ny] == '1' && broken < k && dis[nx][ny][broken + 1] == -1) {
+				q.push({ nx, ny, broken + 1 });
+				dis[nx][ny][broken + 1] = dis[curX][curY][broken] + 1;
+			}
+		}
+	}
+	
+	int ans = INT_MAX;
+	for (int k = 0; k <= 10; k++)
+		if (dis[n - 1][m - 1][k] != -1) ans = min(ans, dis[n - 1][m - 1][k]);
+
+	return (ans != INT_MAX ? ans : -1);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m, k;
+	cin >> n >> m >> k;
+	for (int i = 0; i < n; i++) cin >> board[i];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			fill(dis[i][j], dis[i][j] + 11, -1);
+
+	cout << bfs(n, m, k) << '\n';
+	return 0;
+}

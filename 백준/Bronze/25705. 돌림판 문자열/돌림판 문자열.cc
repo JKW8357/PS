7a36c94bd73c53b995ba28889013c309ead91cf0
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-22 17:13:34 +0900

    Update 돌림판 문자열.cc

--- 백준/Bronze/25705. 돌림판 문자열/돌림판 문자열.cc
+++ 백준/Bronze/25705. 돌림판 문자열/돌림판 문자열.cc
@@ -9,27 +9,28 @@ int main() {
 	string board, target;
 	cin >> n >> board >> m >> target;
 
-	string str;
 	int bs = board.size();
-	int cur = bs - 1;
+	int ts = target.size();
+	int curPos = bs - 1;
 	int idx = 0;
 	int cnt = 0;
 
-	while (idx < target.size()) {
-		int tmp = cur;
-		do {
-			tmp = (tmp + 1) % bs;
-			cnt++;
+	while (idx < ts) {
+		bool found = false;
+		int searchPos = curPos;
 
-			if (board[tmp] == target[idx]) {
-				str += target[idx];
+		do {
+			searchPos = (searchPos + 1) % bs;
+			cnt++;
+			if (board[searchPos] == target[idx]) {
+				found = true;
+				curPos = searchPos;
+				idx++;
 				break;
 			}
-		} while (tmp != cur);
+		} while (searchPos != curPos);
 
-		idx++;
-		cur = tmp;
-		if (str.size() != idx) {
+		if (!found) {
 			cout << -1 << '\n';
 			return 0;
 		}

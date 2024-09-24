1c03a496e13522f9e6cefeca158f9db7e5d4ad89
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-25 00:25:24 +0900

    Update 싸지방에 간 준하.cc

--- 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.cc
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.cc
@@ -2,18 +2,15 @@
 using namespace std;
 typedef tuple<int, int, int> tup;
 
-int cnt[100005];
-priority_queue<int, vector<int>, greater<int>> seats;
-
 class cmp {
 public:
-	bool operator()(tup t1, tup t2) {
-		return get<1>(t1) > get<1>(t2);
-	}
+	bool operator()(tup t1, tup t2) { return get<1>(t1) > get<1>(t2); }
 };
 
+int cnt[100005];
 vector<pair<int, int>> v;
-priority_queue<tup, vector<tup>, cmp> pq;
+priority_queue<int, vector<int>, greater<int>> seats;
+priority_queue<tup, vector<tup>, cmp> endTime;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -31,32 +28,31 @@ int main() {
 	sort(v.begin(), v.end());
 
 	int maxSeats = -1;
-	int cur = 0;
+	int requiredSeats = 0;
+	int seatNumber = 0;
 
 	for (int i = 0; i < n; i++) {
 		int p = v[i].first;
 		int q = v[i].second;
 
-		while (!pq.empty() && get<1>(pq.top()) <= p) {
-			seats.push(get<2>(pq.top()));
-			pq.pop();
+		while (!endTime.empty() && get<1>(endTime.top()) <= p) {
+			seats.push(get<2>(endTime.top()));
+			endTime.pop();
 		}
 
-		int seatNumber;
 		if (!seats.empty()) {
 			seatNumber = seats.top();
 			seats.pop();
 		}
 
-		else seatNumber = ++cur;
+		else seatNumber = ++requiredSeats;
 
-		pq.push({ p, q, seatNumber });
+		endTime.push({ p, q, seatNumber });
 		cnt[seatNumber]++;
 		maxSeats = max(maxSeats, seatNumber);
 	}
 
 	cout << maxSeats << '\n';
 	for (int i = 1; i <= maxSeats; i++) cout << cnt[i] << ' ';
-
 	return 0;
 }

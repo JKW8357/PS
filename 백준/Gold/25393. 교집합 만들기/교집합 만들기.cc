2f595ded16cba72b0d51c5c16f4507d310347dcc
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-27 17:32:23 +0900

    [Gold V] Title: 교집합 만들기, Time: 924 ms, Memory: 39384 KB -BaekjoonHub

---
+++ 백준/Gold/25393. 교집합 만들기/교집합 만들기.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<int, int> maxright;
+map<int, int> minleft;
+set<pair<int, int>> s;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int l, r;
+		cin >> l >> r;
+		s.insert({ l, r });
+
+		maxright[l] = (maxright.count(l) ? max(maxright[l], r) : r);
+		minleft[r] = (minleft.count(r) ? min(minleft[r], l) : l);
+	}
+
+	int q;
+	cin >> q;
+	while (q--) {
+		int l, r;
+		cin >> l >> r;
+
+		if (s.find({ l, r }) != s.end()) {
+			cout << 1 << '\n';
+			continue;
+		}
+
+		if (maxright.count(l) && maxright[l] > r && minleft.count(r) && minleft[r] < l) cout << 2 << '\n';
+		else cout << -1 << '\n';
+	}
+
+	return 0;
+}

0961168fbad4e4ee1b7aa843cde432714bce663b
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-09-29 13:42:40 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 28 ms, Memory: 4828 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+#define W first
+#define V second
+
+int v, e, st, en;
+vector<pii> adj[1005];
+int dis[20005];
+int pre[1005];
+
+void dijkstra() {
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	fill(dis, dis + v + 1, INT_MAX);
+	dis[st] = 0;
+	pq.push({ dis[st],st });
+
+	while (!pq.empty()) {
+		auto cur = pq.top(); pq.pop();
+		if (dis[cur.V] != cur.W) continue;
+
+		for (auto nxt : adj[cur.V]) {
+			if (dis[nxt.V] <= dis[cur.V] + nxt.W) continue;
+			dis[nxt.V] = dis[cur.V] + nxt.W;
+			pre[nxt.V] = cur.V;
+			pq.push({ dis[nxt.V],nxt.V });
+		}
+	}
+}
+
+void trace() {
+	vector<int> path;
+	int cur = en;
+
+	while (cur != st) {
+		path.push_back(cur);
+		cur = pre[cur];
+	}
+
+	path.push_back(cur);
+	reverse(path.begin(), path.end());
+
+	cout << path.size() << '\n';
+	for (auto x : path) cout << x << ' ';
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> v >> e;
+	while (e--) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		adj[u].push_back({ w,v });
+	}
+	cin >> st >> en;
+
+	dijkstra();
+	cout << dis[en] << '\n';
+	trace();
+
+	return 0;
+}

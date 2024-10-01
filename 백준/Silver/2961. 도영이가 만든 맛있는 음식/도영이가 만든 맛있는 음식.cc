a9848ebeb124d9169e32da840657016ecc67f451
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-01 15:16:07 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int ans = INT_MAX;
+vector<pair<int, int>> ingredients;
+
+int popCountll(int num) {
+	int count = 0;
+	while (num) {
+		num &= (num - 1);
+		count++;
+	}
+	return count;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int s, b;
+		cin >> s >> b;
+		ingredients.push_back({ s, b });
+	}
+
+	for (int mask = 1; mask < (1 << n); mask++) {
+		int sour = 1;
+		int bitter = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (mask & (1 << i)) {
+				sour *= ingredients[i].first;
+				bitter += ingredients[i].second;
+			}
+		}
+
+		ans = min(ans, abs(sour - bitter));
+	}
+	
+	cout << ans << '\n';
+	return 0;
+}

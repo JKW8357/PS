c9ed2d185d35d7d47f0b9f107cf3069bc9026127
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-02 02:15:20 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 12 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+queue<int> q;
+int maxCount = 0;
+int lastNum = INT_MAX;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	while (n--) {
+		int cmd;
+		cin >> cmd;
+
+		if (cmd == 1) {
+			int a;
+			cin >> a;
+			q.push(a);
+		}
+
+		else q.pop();
+
+		if (maxCount <= q.size()) {
+			lastNum = (q.size() == maxCount ? min(lastNum, q.back()) : q.back());
+			maxCount = q.size();
+		}
+	}
+
+	cout << maxCount << ' ' << lastNum << '\n';
+	return 0;
+}

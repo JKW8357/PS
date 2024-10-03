4950efb40dfc9da5022a3c7f7c54eed7b34a0ae6
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-03 23:29:46 +0900

    Update 미소녀 컴퓨터 파루빗토 쨩.cc

--- 백준/Silver/28086. 미소녀 컴퓨터 파루빗토 쨩/미소녀 컴퓨터 파루빗토 쨩.cc
+++ 백준/Silver/28086. 미소녀 컴퓨터 파루빗토 쨩/미소녀 컴퓨터 파루빗토 쨩.cc
@@ -5,6 +5,7 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	
 	long long num1, num2, ans;
 	char op;
 	cin >> oct >> num1 >> op >> oct >> num2;
@@ -24,6 +25,14 @@ int main() {
 	cout << oct << abs(ans);
 	return 0;
 }
+
+/*
+고정된 포맷의 입력을 처리할 때는 굳이 문자열을 통해 복잡하게 파싱할 필요가 없이, 
+C++의 입출력 조작자(cin, cout)를 활용하면 훨씬 더 간단하고 효율적으로 파싱할 수 있다.
+
+cin은 기본적으로 공백을 구분자로 사용하지만, 숫자와 문자의 조합을 입력할 때
+cin은 타입에 따라 입력을 숫자와 문자로 구분해 적절한 변수에 값을 저장한다.
+*/
 
 /*
 고정된 포맷의 입력을 처리할 때는 굳이 문자열을 통해 복잡하게 파싱할 필요가 없다.

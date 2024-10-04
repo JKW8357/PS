a1abb258dbe420f5e2301012183733f4fa539213
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-04 18:04:18 +0900

    [Silver V] Title: K-세준수, Time: 12 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1418. K－세준수/K－세준수.cc
+++ 백준/Silver/1418. K－세준수/K－세준수.cc
@@ -3,37 +3,39 @@ using namespace std;
 
 int n, k;
 vector<bool> state(100005, true);
+vector<int> primes;
 
 void sieve() {
 	state[1] = false;
-	for (int i = 2; i <= 100000; i++) {
+	for (int i = 2; i * i <= k; i++) {
 		if (!state[i]) continue;
-		for (int j = i; j <= n; j += i)
-			if (state[j]) state[j] = false;
+		for (int j = i * i; j <= k; j += i)	state[j] = false;
 	}
 }
 
 bool check(int num) {
-	int max_val = 1;
-	for (int i = 2; i <= num; i++) {
-		while (num % i == 0) {
-			max_val = i;
-			num /= i;
+	int maxPrime = 1;
+	for (int prime : primes) {
+		while (num % prime == 0) {
+			maxPrime = prime;
+			num /= prime;
 		}
+		if (num == 1) break;
 	}
-	return max_val <= k;
+	if (num != 1) maxPrime = max(maxPrime, num);
+	return maxPrime <= k;
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	sieve();
 	cin >> n >> k;
+	sieve();
+	for (int i = 2; i <= k; i++) if (state[i]) primes.push_back(i);
 
 	int cnt = 1;
-	for (int i = 1; i <= n; i++)
-		if (state[i] && check(i)) cnt++;
+	for (int i = 2; i <= n; i++) if (check(i)) cnt++;
 	cout << cnt << '\n';
 
 	return 0;

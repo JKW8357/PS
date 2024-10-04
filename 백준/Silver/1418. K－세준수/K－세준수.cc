c87d4c8b266f43044d164dfee00edc9e0d42453f
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-04 17:31:41 +0900

    [Silver V] Title: K-세준수, Time: 1788 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1418. K－세준수/K－세준수.cc
+++ 백준/Silver/1418. K－세준수/K－세준수.cc
@@ -2,6 +2,17 @@
 using namespace std;
 
 int n, k;
+vector<bool> state(100005, true);
+
+void sieve() {
+	state[1] = false;
+	for (int i = 2; i <= 100000; i++) {
+		if (!state[i]) continue;
+		for (int j = i; j <= n; j += i)
+			if (state[j]) state[j] = false;
+	}
+}
+
 bool check(int num) {
 	int max_val = 1;
 	for (int i = 2; i <= num; i++) {
@@ -17,10 +28,12 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	sieve();
 	cin >> n >> k;
-	int cnt = 0;
+
+	int cnt = 1;
 	for (int i = 1; i <= n; i++)
-		if (check(i)) cnt++;
+		if (state[i] && check(i)) cnt++;
 	cout << cnt << '\n';
 
 	return 0;

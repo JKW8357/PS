36390fce7360aded163b088bac69c1ffa3c63dd2
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-08 22:36:12 +0900

    [Silver II] Title: 아카라카, Time: 64 ms, Memory: 11988 KB -BaekjoonHub

--- 백준/Silver/23304. 아카라카/아카라카.cc
+++ 백준/Silver/23304. 아카라카/아카라카.cc
@@ -2,19 +2,15 @@
 using namespace std;
 
 bool akaraka(string& str) {
-	if (str.size() == 1) return true;
-
 	int size = str.size();
+	if (size == 1) return true;
+
+	string tmp(str);
+	reverse(tmp.begin(), tmp.end());
+	if (str != tmp) return false;
 
 	string s1 = str.substr(0, size / 2);
-	string revS1 = s1;
-	reverse(revS1.begin(), revS1.end());
-
 	string s2 = str.substr(size - size / 2, size);
-	string revS2 = s2;
-	reverse(revS2.begin(), revS2.end());
-
-	if (s1 != revS1 || s2 != revS2) return false;
 
 	return (akaraka(s1) && akaraka(s2));
 }

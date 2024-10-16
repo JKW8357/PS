10bc43b04e06fef04863057f2f2bc655c0034523
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-16 19:24:11 +0900

    [Silver III] Title: 슬라임 합치기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14241. 슬라임 합치기/슬라임 합치기.cc
+++ 백준/Silver/14241. 슬라임 합치기/슬라임 합치기.cc
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[105];
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) cin >> arr[i];
 
 	int score = 0;
-	for (int i = 0; i < n - 1; i++)
-		for (int j = i + 1; j < n; j++)
-			score += arr[i] * arr[j];
+	int a; cin >> a;
+	for (int i = 0; i < n - 1; i++) {
+		int b; cin >> b;
+		score += a * b;
+		a += b;
+	}
 	
 	cout << score << '\n';
 	return 0;

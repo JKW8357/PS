ba205a5259f73344501b78d3ed09308fb19e1857
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-16 23:44:16 +0900

    Update 팩토리얼 3.py

--- 백준/Bronze/27434. 팩토리얼 3/팩토리얼 3.py
+++ 백준/Bronze/27434. 팩토리얼 3/팩토리얼 3.py
@@ -1,10 +1,6 @@
 import sys
 import math
-sys.set_int_max_str_digits(0)
-
 input = sys.stdin.readline
-print = sys.stdout.write
 
 n = int(input())
-ans = math.factorial(n)
-print(str(ans) + '\n')
+print(math.factorial(n))

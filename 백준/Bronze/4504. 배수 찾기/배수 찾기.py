c217d8713bba120a903299ca163d1f763642c629
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-17 23:22:51 +0900

    [Bronze III] Title: 배수 찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4504. 배수 찾기/배수 찾기.py
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.py
@@ -4,5 +4,6 @@ input = sys.stdin.readline
 n = int(input())
 while True:
     num = int(input())
-    if num == 0: break
+    if num == 0:
+        break
     print(f"{num} is {'NOT ' if num % n != 0 else ''}a multiple of {n}.")

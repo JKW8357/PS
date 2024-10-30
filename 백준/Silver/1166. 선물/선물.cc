e5a29b02c8caaf04a42a94c24585a24d15866312
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-30 23:36:19 +0900

    [Silver III] Title: 선물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1166. 선물/선물.cc
+++ 백준/Silver/1166. 선물/선물.cc
@@ -17,7 +17,7 @@ int main() {
 	// 매개변수가 정수가 아닌 경우에도 매개변수 탐색이 가능하다.
 	// 다만 부동 소수점 오차를 고려해야 함에 주의한다.
 	double lo = 0, hi = 1e9;
-	for (int i = 0; i < 1000; i++) {
+	for (int i = 0; i < 100; i++) {
 		double mid = midpoint(lo, hi);
 		if (param(mid)) lo = mid;
 		else hi = mid;

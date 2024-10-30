3755d06157bae3acc0a3c69dcbc5daf3c32c7ca9
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-10-30 19:33:39 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 296 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -7,17 +7,15 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n;
-	cin >> n;
+	int n; cin >> n;
 	for (int i = 0; i < n; i++) cin >> arr[i];
 	sort(arr, arr + n);
 
-	int m;
-	cin >> m;
-	while(m--) {
-		int q;
-		cin >> q;
-		cout << upper_bound(arr, arr + n, q) - lower_bound(arr, arr + n, q) << ' ';
+	int m; cin >> m;
+	while (m--) {
+		int num; cin >> num;
+		int cnt = upper_bound(arr, arr + n, num) - lower_bound(arr, arr + n, num);
+		cout << cnt << ' ';
 	}
 
 	return 0;

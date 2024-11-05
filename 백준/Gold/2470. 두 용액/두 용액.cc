763941a6f9fa53e740dc54ec4283b9717314d441
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-11-05 19:22:36 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[100005];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr, arr + n);
+
+	int l = 0, r = n - 1;
+	int minVal = INT_MAX;
+	int ans1, ans2;
+	while (l < r) {
+		int sum = arr[l] + arr[r];
+		if (minVal > abs(sum)) {
+			minVal = min(minVal, abs(sum));
+			ans1 = arr[l];
+			ans2 = arr[r];
+		}
+		sum > 0 ? r-- : l++;
+	}
+	cout << ans1 << ' ' << ans2;
+
+	return 0;
+}

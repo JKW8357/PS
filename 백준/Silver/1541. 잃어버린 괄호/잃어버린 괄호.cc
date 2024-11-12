f77fa3592309d63bb569d7d3dac99ec93deea0cc
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-11-12 21:26:37 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -5,26 +5,22 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	string str;
-	cin >> str;
+	string str; cin >> str;
+	bool isMinus = false;
+	int ans = 0, num = 0;
+	string tmp;
 
-	int ans = 0, tmp = 0;
-	int size = str.size();
-	bool isminus = false;
-
-	for (int i = 0; i <= size; ++i) {
-		if (i == size || str[i] == '+' || str[i] == '-') {
-
-			int num = stoi(str.substr(tmp, i - tmp));
-
-			if (isminus) ans -= num;
-			else ans += num;
-
-			tmp = i + 1;
-			if (str[i] == '-') isminus = true;
+	for (int i = 0; i <= str.size(); i++) {
+		if (i == str.size() || !isdigit(str[i])) {
+			num = stoi(tmp);
+			ans += isMinus ? -num : num;
+			tmp.clear();
+			num = 0;
 		}
+		else if (isdigit(str[i])) tmp += str[i];
+		if (str[i] == '-') isMinus = true;
 	}
 
-	cout << ans;
+	cout << ans << '\n';
 	return 0;
 }

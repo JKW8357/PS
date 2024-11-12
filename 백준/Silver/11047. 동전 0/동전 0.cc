76407227263e44fdf6fa11752153bf93bcb4ee97
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2024-11-12 19:34:01 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	int cnt = 0;
+	for (int i = n - 1; i >= 0; i--) {
+		if (k == 0) break;
+		if (k < arr[i]) continue;
+		int moneyCnt = k / arr[i];
+		cnt += k / arr[i];
+		k %= arr[i];
+	}
+	
+	cout << cnt << '\n';
+	return 0;
+}

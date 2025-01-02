694609828a1b54f9127e2bbc03fb82709fdb6c27
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-03 03:59:21 +0900

    [Bronze I] Title: 선로에 마네킹이야!!, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15920. 선로에 마네킹이야！！/선로에 마네킹이야！！.py
+++ 백준/Bronze/15920. 선로에 마네킹이야！！/선로에 마네킹이야！！.py
@@ -1,25 +1,20 @@
 n = int(input())
 s = input()
 
-state = 0
-pushed = False
-multitrack_drifting = False
-result = 0
-
+state, pushed, drift = 0, False, False
 for command in s:
     if command == 'P':
         pushed = not pushed
         if state == 1:
-            multitrack_drifting = True
-
+            drift = True
     else:
         state += 1
-
     if state == 2:
-        if multitrack_drifting:
-            result = 6
-        else:
-            result = 1 if pushed else 5
         break
 
-print(result)
+if state < 2:
+    print(0)
+elif drift:
+    print(6)
+else:
+    print(1 if pushed else 5)

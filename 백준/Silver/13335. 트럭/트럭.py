24bf57f72d2864eac615dc0a044d07062d647efe
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-03 20:39:04 +0900

    [Silver I] Title: 트럭, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -1,17 +1,15 @@
-from collections import deque
-
 n, w, l = map(int, input().split())
-dq = deque(list(map(int, input().split())))
+trucks = list(map(int, input().split()))
 
 time = 0
-bridge = deque([0] * w)
+bridge = [0] * w
 
-while dq or bridge:
+while bridge:
     time += 1
-    bridge.popleft()
-    if dq:
-        if sum(bridge) + dq[0] <= l:
-            bridge.append(dq.popleft())
+    bridge.pop(0)
+    if trucks:
+        if sum(bridge) + trucks[0] <= l:
+            bridge.append(trucks.pop(0))
         else:
             bridge.append(0)
 

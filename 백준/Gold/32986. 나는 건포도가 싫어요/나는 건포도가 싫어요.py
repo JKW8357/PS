042760952c64bc588dbdf8335ff0e49318de189d
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-05 12:51:47 +0900

    [Gold V] Title: 나는 건포도가 싫어요, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/32986. 나는 건포도가 싫어요/나는 건포도가 싫어요.py
+++ 백준/Gold/32986. 나는 건포도가 싫어요/나는 건포도가 싫어요.py
@@ -1,5 +1,2 @@
 x, y, z = map(int, input().split())
-if x == 3 and y == 3 and z == 3:
-    print(0)
-else:
-    print((min(x, y, z) - 1) // 2)
+print(0 if x == 3 and y == 3 and z == 3 else (min(x, y, z) - 1) // 2)

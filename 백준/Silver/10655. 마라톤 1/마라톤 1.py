8bfd357d7c20a90ca0ab00d13df93afb22115c55
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-12 05:37:57 +0900

    [Silver III] Title: 마라톤 1, Time: 2792 ms, Memory: 50360 KB -BaekjoonHub

--- 백준/Silver/10655. 마라톤 1/마라톤 1.py
+++ 백준/Silver/10655. 마라톤 1/마라톤 1.py
@@ -1,15 +1,12 @@
 n = int(input())
-checkpoint = [tuple(map(int, input().split())) for i in range(n)]
+checkpoint = [tuple(map(int, input().split())) for _ in range(n)]
 
-def manhattan(p1, p2):
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-
-distances = [manhattan(checkpoint[i], checkpoint[i - 1]) for i in range(1, n)]
+distances = [abs(checkpoint[i][0] - checkpoint[i - 1][0]) + abs(checkpoint[i][1] - checkpoint[i - 1][1]) for i in range(1, n)]
 sum_distances = sum(distances)
 ans = sum_distances
 
 for i in range(1, n - 1):
-    candidate = sum_distances - distances[i - 1] - distances[i] + manhattan(checkpoint[i + 1], checkpoint[i - 1])
+    candidate = sum_distances - distances[i - 1] - distances[i] + abs(checkpoint[i + 1][0] - checkpoint[i - 1][0]) + abs(checkpoint[i + 1][1] - checkpoint[i - 1][1])
     ans = min(ans, candidate)
 
 print(ans)

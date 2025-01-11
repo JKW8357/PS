30cde7ae416a460bb21df3fcb947525fc1d9f95c
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-12 03:58:30 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 104 ms, Memory: 14876 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.cc
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.cc
@@ -9,35 +9,51 @@ int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
 
 string board[1005];
-bool vis[1005][1005];
-pair<int, int> zeros[1005][1005];
+int p[1000005]; // 좌표를 저장하는 Disjoint-Set
+int areaSize[1000005];
 int output[1005][1005];
-int areaNumber = 1;
+
+int coordToId(int x, int y) {
+	return x * m + y; // 2D 좌표를 1D ID로 변환
+}
+
+int find(int x) {
+	if (p[x] == x) return x;
+	return p[x] = find(p[x]);
+}
+
+void unite(int a, int b) {
+	a = find(a);
+	b = find(b);
+	if (a != b) {
+		p[b] = a;
+		areaSize[a] += areaSize[b];
+	}
+}
 
 void bfs(int x, int y) {
 	queue<pair<int, int>> q;
-	vector<pair<int, int>> zeros_coord = {{ x, y }};
 	q.push({ x, y });
-	vis[x][y] = true;
-	int cnt = 1;
+	int rootId = coordToId(x, y); // 현재 영역의 루트 ID
+	p[rootId] = rootId;
+	areaSize[rootId] = 1;
+	board[x][y] = '2'; // 별도의 vis 배열을 사용하지 않고 방문 처리
 
 	while (!q.empty()) {
 		auto cur = q.front(); q.pop();
 		for (int dir = 0; dir < 4; dir++) {
 			int nx = cur.X + dx[dir];
 			int ny = cur.Y + dy[dir];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (vis[nx][ny] || board[nx][ny] == '1') continue;
-			q.push({ nx, ny });
-			vis[nx][ny] = true;
-			zeros_coord.push_back({ nx, ny });
-			cnt++;
-		}
-	}
 
-	areaNumber++;
-	for (const auto& e : zeros_coord) {
-		zeros[e.X][e.Y] = { areaNumber, cnt };
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+			if (board[nx][ny] != '0') continue;
+
+			q.push({ nx, ny });
+			board[nx][ny] = '2';
+			int neighborId = coordToId(nx, ny);
+			p[neighborId] = rootId; // 같은 영역으로 합침
+			areaSize[rootId]++;
+		}
 	}
 }
 
@@ -50,22 +66,27 @@ int main() {
 	
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
-			if (!vis[i][j] && board[i][j] == '0') bfs(i, j);
+			if (board[i][j] == '0') bfs(i, j);
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			if (board[i][j] == '1') {
 				output[i][j] = 1;
-				vector<int> used;
+				set<int> used;
+
 				for (int dir = 0; dir < 4; dir++) {
 					int nx = i + dx[dir];
 					int ny = j + dy[dir];
 					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-					int currentAreaNumber = zeros[nx][ny].first;
-					if (currentAreaNumber == 0 || find(used.begin(), used.end(), currentAreaNumber) != used.end()) continue;
-					used.push_back(currentAreaNumber);
-					output[i][j] += zeros[nx][ny].second;
+
+					if (board[nx][ny] == '2') { // 방문한 0 영역만 고려
+						int rootId = find(coordToId(nx, ny));
+						if (used.count(rootId)) continue;
+						used.insert(rootId);
+						output[i][j] += areaSize[rootId];
+					}
 				}
+
 				output[i][j] %= 10;
 			}
 		}

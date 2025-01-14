a937d6610f8a45c1390274fef4db1edc51b735a4
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-15 04:48:52 +0900

    [Gold IV] Title: 알파벳, Time: 336 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -4,17 +4,17 @@ using namespace std;
 int r, c;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-string board[20];
+int board[20][20];
 
 int solve(int x, int y, int state) {
 	int result = 1;
-	state |= (1 << (board[x][y] - 'A'));
+	state |= (1 << board[x][y]);
 
 	for (int dir = 0; dir < 4; dir++) {
 		int nx = x + dx[dir];
 		int ny = y + dy[dir];
 		if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-		if (!(state & (1 << (board[nx][ny] - 'A'))))
+		if (!(state & (1 << board[nx][ny])))
 			result = max(result, 1 + solve(nx, ny, state));
 	}
 
@@ -26,7 +26,12 @@ int main() {
 	cin.tie(NULL);
 
 	cin >> r >> c;
-	for (int i = 0; i < r; i++) cin >> board[i];
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			char ch; cin >> ch;
+			board[i][j] = ch - 'A';
+		}
+	} 
 
 	int ans = solve(0, 0, 0);
 	cout << ans << '\n';

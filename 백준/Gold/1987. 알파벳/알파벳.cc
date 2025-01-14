def0ae83ac646c468b63a6171c6aaa6f9b355a03
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-15 04:43:44 +0900

    [Gold IV] Title: 알파벳, Time: 368 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -2,27 +2,23 @@
 using namespace std;
 
 int r, c;
-int ans = 0;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-
 string board[20];
-int dis[20][20];
-bool alphabet[26];
 
-void dfs(int x, int y) {
-	ans = max(ans, dis[x][y]);
+int solve(int x, int y, int state) {
+	int result = 1;
+	state |= (1 << (board[x][y] - 'A'));
+
 	for (int dir = 0; dir < 4; dir++) {
 		int nx = x + dx[dir];
 		int ny = y + dy[dir];
 		if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-		if (dis[nx][ny] != 0 || alphabet[board[nx][ny] - 'A']) continue;
-		dis[nx][ny] = dis[x][y] + 1;
-		alphabet[board[nx][ny] - 'A'] = true;
-		dfs(nx, ny);
-		dis[nx][ny] = 0;
-		alphabet[board[nx][ny] - 'A'] = false;
+		if (!(state & (1 << (board[nx][ny] - 'A'))))
+			result = max(result, 1 + solve(nx, ny, state));
 	}
+
+	return result;
 }
 
 int main() {
@@ -32,10 +28,8 @@ int main() {
 	cin >> r >> c;
 	for (int i = 0; i < r; i++) cin >> board[i];
 
-	dis[0][0] = 1;
-	alphabet[board[0][0] - 'A'] = true;
-	dfs(0, 0);
-
+	int ans = solve(0, 0, 0);
 	cout << ans << '\n';
+
 	return 0;
 }

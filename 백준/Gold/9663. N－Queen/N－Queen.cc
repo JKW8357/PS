abe26659b758b6eb30c00d3a78d072e1af9113db
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-15 23:10:32 +0900

    [Gold IV] Title: N-Queen, Time: 1604 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,9 +3,7 @@ using namespace std;
 
 int n;
 int ans = 0;
-bool isused1[40]; // 열 방향 탐색
-bool isused2[40]; // 대각선 탐색 1
-bool isused3[40]; // 대각선 탐색 2
+int col, diag1, diag2;
 
 void solve(int depth) {
 	if (depth == n) {
@@ -14,14 +12,16 @@ void solve(int depth) {
 	}
 
 	for (int i = 0; i < n; i++) {
-		if (isused1[i] || isused2[i - depth + n - 1] || isused3[i + depth]) continue;
-		isused1[i] = true;
-		isused2[i - depth + n - 1] = true;
-		isused3[i + depth] = true;
+		if (col & (1 << i) || diag1 & (1 << (i - depth + n - 1)) || diag2 & (1 << (i + depth))) continue;
+		col |= (1 << i);
+		diag1 |= (1 << (i - depth + n - 1));
+		diag2 |= (1 << (i + depth));
+
 		solve(depth + 1);
-		isused1[i] = false;
-		isused2[i - depth + n - 1] = false;
-		isused3[i + depth] = false;
+
+		col &= ~(1 << i);
+		diag1 &= ~(1 << (i - depth + n - 1));
+		diag2 &= ~(1 << (i + depth));
 	}
 }
 

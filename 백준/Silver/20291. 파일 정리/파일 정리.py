d3b273af05369dfc34b8e1ea414356b872f41c21
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-19 02:29:34 +0900

    [Silver III] Title: 파일 정리, Time: 252 ms, Memory: 44716 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,10 +1,12 @@
+import sys
 from collections import Counter
+input = sys.stdin.readline
 
 n = int(input())
 extensions = Counter()
 
 for _ in range(n):
-    extension = input().split('.')[1]
+    extension = input().rstrip().split('.')[1]
     extensions[extension] += 1
 
 for key, value in sorted(extensions.items()):

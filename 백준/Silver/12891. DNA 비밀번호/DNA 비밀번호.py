f6ca84fde2f6e02c3ce550ab62c136bb746a4d5c
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-19 04:18:09 +0900

    [Silver II] Title: DNA 비밀번호, Time: 632 ms, Memory: 34344 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -1,14 +1,15 @@
-from collections import Counter
-
 s, t = map(int, input().split())
 dna_string = input()
-minimum_count = Counter({nucleotide : count for nucleotide, count in zip(['A', 'C', 'G', 'T'], map(int, input().split()))})
-substring_count = Counter(dna_string[:t])
-cnt = 1 if substring_count >= minimum_count else 0
+minimum_count = {nucleotide : count for nucleotide, count in zip(['A', 'C', 'G', 'T'], map(int, input().split()))}
+
+substring_count = {char: 0 for char in 'ACGT'}
+for char in dna_string[:t]:
+    substring_count[char] += 1
+cnt = 1 if all(substring_count[nucleotide] >= minimum_count[nucleotide] for nucleotide in 'ACGT') else 0
 
 for i in range(1, s - t + 1):
     substring_count[dna_string[i + t - 1]] += 1
     substring_count[dna_string[i - 1]] -= 1
-    if substring_count >= minimum_count: cnt += 1
+    if all(substring_count[nucleotide] >= minimum_count[nucleotide] for nucleotide in 'ACGT'): cnt += 1
 
 print(cnt)

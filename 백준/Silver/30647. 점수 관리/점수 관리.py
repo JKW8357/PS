d6c7048f5437f02f629c515879527cebb44f6dac
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-19 06:41:28 +0900

    Update 점수 관리.py

--- 백준/Silver/30647. 점수 관리/점수 관리.py
+++ 백준/Silver/30647. 점수 관리/점수 관리.py
@@ -3,8 +3,10 @@ n = int(input())
 table = sorted(eval(sys.stdin.read()), key=lambda x: (-x['score'], x['name']))
 
 rank = 0
-for i in range(len(table)):
-    if i == 0 or table[i]['score'] != table[i - 1]['score']:
+prev_student = table[0]
+for i, student in enumerate(table):
+    if i == 0 or student['score'] != prev_student['score']:
         rank = max(rank + 1, i + 1)
-    if table[i]['isHidden']: continue
-    print(rank, table[i]['name'], table[i]['score'])
+        prev_student = student
+    if student['isHidden']: continue
+    print(rank, student['name'], student['score'])

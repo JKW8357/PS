b5fe876108cc81cb824b20d1856c4af572ce0c29
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-23 04:36:48 +0900

    [Silver V] Title: 다각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11896. 다각형/다각형.py
+++ 백준/Silver/11896. 다각형/다각형.py
@@ -1,9 +1,7 @@
 a, b = map(int, input().split())
-ans = 0
 if b < 4:
-    ans = 0
+    print(0)
 else:
     a = max(a, 4) + (max(a, 4) % 2)
     b -= b % 2
-    ans = ((b - a) // 2 + 1) * (a + b) // 2
-print(ans)
+    print(((b - a) // 2 + 1) * (a + b) // 2)

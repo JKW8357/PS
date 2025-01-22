f06a6658451c40fc8fd4570072f4fa54058b90e0
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-22 20:54:54 +0900

    [Silver I] Title: 2-SAT - 2, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11278. 2－SAT － 2/2－SAT － 2.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<pair<int, int>> clauses;
+
+bool check(int x, int y, int state) {
+	bool a = (x > 0 ? (state & (1 << (x - 1))) != 0 : (state & (1 << (-x - 1))) == 0);
+	bool b = (y > 0 ? (state & (1 << (y - 1))) != 0 : (state & (1 << (-y - 1))) == 0);
+	return a || b;
+}
+
+bool solve(int state) {
+	for (auto& clause : clauses)
+		if (!check(clause.first, clause.second, state))
+			return false;
+	return true;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> n >> m;
+	clauses.resize(m);
+	for (int i = 0; i < m; i++)
+		cin >> clauses[i].first >> clauses[i].second;
+
+	bool canSolve = false;
+	for (int bitmask = 0; bitmask < (1 << n); bitmask++) {
+		if (solve(bitmask)) {
+			cout << 1 << '\n';
+			for (int i = 0; i < n; i++)
+				cout << ((bitmask & (1 << i) ? 1 : 0)) << ' ';
+			return 0;
+		}
+	}
+
+	cout << 0 << '\n';
+	return 0;
+}

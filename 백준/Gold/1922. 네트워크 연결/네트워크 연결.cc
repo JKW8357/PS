716549f2dafb2a717d87688e8736e67a7bbb9a1e
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-26 05:03:37 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 3196 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+tuple<int, int, int> adj[100005];
+vector<int> p(1005, -1);
+
+int find(int x) {
+	if (p[x] == -1) return x;
+	return p[x] = find(p[x]);
+}
+
+bool unite(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y) return false;
+	p[y] = x;
+	return true;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		adj[i] = { c, a, b };
+	}
+
+	sort(adj, adj + m);
+
+	int cnt = 0, ans = 0;
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		tie(c, a, b) = adj[i];
+		if (!unite(a, b)) continue;
+		ans += c;
+		cnt++;
+		if (cnt == n - 1) break;
+	}
+
+	cout << ans << '\n';
+}

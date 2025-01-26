4c9a6d8d08e41c7ab36d3a8d93c18ceb1401cee4
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-26 12:48:03 +0900

    [Gold III] Title: 별자리 만들기, Time: 0 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+
+vector<pair<double, double>> coord;
+vector<tuple<double, double, double>> adj;
+vector<int> p(100005, -1);
+
+int find(int x) {
+	if (p[x] == -1) return x;
+	return p[x] = find(p[x]);
+}
+
+bool unite(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y) return false;
+	p[y] = x;
+	return true;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n; cin >> n;
+	coord.resize(n);
+	for (int i = 0; i < n; i++) cin >> coord[i].X >> coord[i].Y;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			double dx = coord[j].X - coord[i].X;
+			double dy = coord[j].Y - coord[i].Y;
+			double dist = sqrt(dx * dx + dy * dy);
+			adj.push_back({ dist, i, j });
+		}
+	}
+
+	sort(adj.begin(), adj.end());
+
+	int cnt = 0;
+	double ans = 0;
+	for (auto& e : adj) {
+		double dist;
+		int i, j;
+		tie(dist, i, j) = e;
+		if (!unite(i, j)) continue;
+		ans += dist;
+		cnt++;
+		if (cnt == n - 1) break;
+	}
+
+	cout << fixed << setprecision(2) << ans << '\n';
+	return 0;
+}

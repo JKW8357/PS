20bfdc29b9c8baa70a8055591ad0f8a48fe5c977
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-01-31 23:33:13 +0900

    [Gold V] Title: 회문 끝말잇기, Time: 336 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/30641. 회문 끝말잇기/회문 끝말잇기.py
+++ 백준/Gold/30641. 회문 끝말잇기/회문 끝말잇기.py
@@ -3,14 +3,22 @@ input = sys.stdin.readline
 MOD = 10 ** 9 + 7
 
 l, u = map(int, input().split())
-
 cnt = 0
-while l < 3 and l <= u:
+multi = 1
+
+if l == 1:
     cnt += 1
     l += 1
+if u != 1 and l == 2:
+    cnt += 1
+    l += 1
+
 print('H' if cnt % 2 == 1 else 'A')
 
-for i in range(l, u + 1):
-    cnt += pow(26, (i - 1) // 2, MOD)
-    cnt %= MOD
+for i in range(3, u + 1):
+    if i % 2 == 1:
+        multi = (multi * 26) % MOD
+    if i >= l:
+        cnt = (cnt + multi) % MOD
+
 print(cnt)

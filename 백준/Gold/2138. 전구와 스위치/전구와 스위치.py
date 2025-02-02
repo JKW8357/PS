4c26538775466968eca044575d1744dfb2282cea
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-02 22:19:36 +0900

    [Gold IV] Title: 전구와 스위치, Time: 132 ms, Memory: 39880 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -5,22 +5,20 @@ n = int(input())
 current_state = list(input())
 target_state = list(input())
 
-def check(current_state, is_toggled):
+def check(bulbs, is_toggled):
     cnt = 0
     if is_toggled:
-        current_state[0] = str(1 - int(current_state[0]))
-        current_state[1] = str(1 - int(current_state[1]))
+        bulbs[0] = str(1 - int(bulbs[0]))
+        bulbs[1] = str(1 - int(bulbs[1]))
         cnt += 1
 
     for i in range(1, n):
-        if current_state[i - 1] != target_state[i - 1]:
+        if bulbs[i - 1] != target_state[i - 1]:
             for j in range(i - 1, min(i + 2, n)):
-                current_state[j] = str(1 - int(current_state[j]))
+                bulbs[j] = str(1 - int(bulbs[j]))
             cnt += 1
 
-    if current_state[n - 1] != target_state[n - 1]:
-        cnt = -1
-    return cnt
+    return cnt if bulbs[n - 1] == target_state[n - 1] else -1
 
 t1 = check(current_state.copy(), True)
 t2 = check(current_state.copy(), False)

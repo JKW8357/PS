8cd5645ce5f62cc339b362d7646c2c98d55e0a08
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-04 14:27:13 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -1,15 +1,8 @@
-from functools import cmp_to_key
 import sys
 input = sys.stdin.readline
 
-def compare(a, b):
-    return -1 if a + b > b + a else 1
-
 n = int(input())
 numbers = list(input().split())
-
-if all(number == '0' for number in numbers):
-    print(0)
-else:
-    numbers.sort(key=cmp_to_key(compare))
-    print(*numbers, sep='')
+numbers.sort(key=lambda x: x * 10, reverse=True)
+result = ''.join(numbers)
+print(result if result[0] != '0' else 0)

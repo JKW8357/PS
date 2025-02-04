22e586f4d1a022545db258a4e17a118919c56b53
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-04 10:50:16 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 68 ms, Memory: 36068 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -3,15 +3,12 @@ import sys
 input = sys.stdin.readline
 
 def compare(a, b):
-    if str(a) + str(b) > str(b) + str(a):
-        return -1
-    else:
-        return 1
+    return -1 if a + b > b + a else 1
 
 n = int(input())
-numbers = list(map(int, input().split()))
+numbers = list(input().split())
 
-if all(number == 0 for number in numbers):
+if all(number == '0' for number in numbers):
     print(0)
 else:
     numbers.sort(key=cmp_to_key(compare))

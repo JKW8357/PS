1b77db8c3861bec7accd9e281435ae0228d4a605
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-04 22:22:02 +0900

    [Gold II] Title: 컵라면, Time: 652 ms, Memory: 63272 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+tasks = list(tuple(map(int, input().split())) for _ in range(n))
+max_day = max(d for d, w in tasks) if tasks else 0
+pq = []
+
+tasks.sort()
+
+ans = 0
+for day in range(max_day, 0, -1):
+    while tasks and tasks[-1][0] >= day:
+        heapq.heappush(pq, -tasks.pop()[1])
+    if pq:
+        ans += -heapq.heappop(pq)
+
+print(ans)

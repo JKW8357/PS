070f374d9be9e06fa62ab24e188429e4973f618d
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-05 17:46:05 +0900

    [Gold IV] Title: 나비의 간식을 훔쳐먹은 춘배, Time: 496 ms, Memory: 114136 KB -BaekjoonHub

--- 백준/Gold/30407. 나비의 간식을 훔쳐먹은 춘배/나비의 간식을 훔쳐먹은 춘배.py
+++ 백준/Gold/30407. 나비의 간식을 훔쳐먹은 춘배/나비의 간식을 훔쳐먹은 춘배.py
@@ -1,33 +1,26 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 hp, dist, k = map(int, input().split())
 punch = [int(input()) for _ in range(n)]
 
-def solve(hp, dist, punch_idx, used_dodge):
-    if punch_idx >= n:
-        return hp
+def solve(hp, dist, i, used_dodge):
+    if i >= n: return hp
 
-    # skill1
-    damage = max(punch[punch_idx] - dist, 0) // 2
-    if used_dodge == 1:
-        used_skill1 = solve(hp, dist, punch_idx + 1, -1)
-    else:
-        used_skill1 = solve(hp - damage, dist, punch_idx + 1, used_dodge)
+    next_dodge = -1 if used_dodge == 1 else used_dodge
+    damage = lambda d: 0 if used_dodge == 1 else max(punch[i] - d, 0)
 
-    # skill2
-    damage = max(punch[punch_idx] - (dist + k), 0)
-    if used_dodge == 1:
-        used_skill2 = solve(hp, dist + k, punch_idx + 1, -1)
-    else:
-        used_skill2 = solve(hp - damage, dist + k, punch_idx + 1, used_dodge)
+    dmg1 = damage(dist) // 2
+    ans1 = solve(hp - dmg1, dist, i + 1, next_dodge)
 
-    # skill3
-    damage = max(punch[punch_idx] - dist, 0)
-    if used_dodge == 0:
-        used_skill3 = solve(hp - damage, dist, punch_idx + 1, 1)
-    else:
-        used_skill3 = 0
+    dmg2 = damage(dist + k)
+    ans2 = solve(hp - dmg2, dist + k, i + 1, next_dodge)
 
-    return max(used_skill1, used_skill2, used_skill3)
+    dmg3 = damage(dist)
+    ans3 = solve(hp - dmg3, dist, i + 1, 1) if used_dodge == 0 else 0
+
+    return max(ans1, ans2, ans3)
 
 max_hp = solve(hp, dist, 0, 0)
 print(max_hp if max_hp > 0 else -1)

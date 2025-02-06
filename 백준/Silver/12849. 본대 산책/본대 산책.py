b2ac6cd48cae34c2250d7bdec032d8715a47d13d
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-07 00:52:04 +0900

    [Silver I] Title: 본대 산책, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12849. 본대 산책/본대 산책.py
+++ 백준/Silver/12849. 본대 산책/본대 산책.py
@@ -2,22 +2,39 @@ import sys
 input = sys.stdin.readline
 MOD = 10 ** 9 + 7
 
+def multiply(m1, m2):
+    n = len(m1)
+    m = len(m1[0])
+    p = len(m2[0])
+
+    result = [[0] * p for _ in range(n)]
+    for i in range(n):
+        for j in range(p):
+            for k in range(m):
+                result[i][j] += m1[i][k] * m2[k][j]
+                result[i][j] %= MOD
+
+    return result
+
+def power(m, n):
+    result = [[1], [0], [0], [0], [0], [0], [0], [0]]
+    while n:
+        if n % 2:
+            result = multiply(m, result)
+            n -= 1
+        m = multiply(m, m)
+        n //= 2
+    return result
+
 d = int(input())
-dp = [0] * 8
-dp[0] = 1
+m = [[0, 1, 1, 0, 0, 0, 0, 0],
+     [1, 0, 1, 1, 0, 0, 0, 0],
+     [1, 1, 0, 1, 1, 0, 0, 0],
+     [0, 1, 1, 0, 1, 1, 0, 0],
+     [0, 0, 1, 1, 0, 1, 1, 0],
+     [0, 0, 0, 1, 1, 0, 0, 1],
+     [0, 0, 0, 0, 1, 0, 0, 1],
+     [0, 0, 0, 0, 0, 1, 1, 0]]
 
-for _ in range(d):
-    tmp = [0] * 8
-    tmp[0] = dp[1] + dp[2]
-    tmp[1] = dp[0] + dp[2] + dp[3]
-    tmp[2] = dp[0] + dp[1] + dp[3] + dp[4]
-    tmp[3] = dp[1] + dp[2] + dp[4] + dp[5]
-    tmp[4] = dp[2] + dp[3] + dp[5] + dp[6]
-    tmp[5] = dp[3] + dp[4] + dp[7]
-    tmp[6] = dp[4] + dp[7]
-    tmp[7] = dp[5] + dp[6]
-
-    for i in range(8):
-        dp[i] = tmp[i] % MOD
-
-print(dp[0])
+ans = sum(power(m, d)[0]) % MOD
+print(ans)

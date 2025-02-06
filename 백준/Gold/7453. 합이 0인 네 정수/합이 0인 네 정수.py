84806f16159f86cc4ed0716ddeceb54f64a2e848
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-06 12:00:21 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 6824 ms, Memory: 649692 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -1,15 +1,39 @@
-from collections import Counter
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 A, B, C, D = [], [], [], []
 
-for i in range(n):
+for _ in range(n):
     a, b, c, d = map(int, input().split())
     A.append(a); B.append(b); C.append(c); D.append(d)
 
-segment_sum = Counter(A[i] + B[j] for i in range(n) for j in range(n))
-ans = sum(segment_sum[-(C[i] + D[j])] for i in range(n) for j in range(n))
+AB = [A[i] + B[j] for i in range(n) for j in range(n)]
+CD = [C[i] + D[j] for i in range(n) for j in range(n)]
+
+AB.sort()
+CD.sort(reverse=True)
+
+i, j = 0, 0
+ans = 0
+len_AB = len(AB)
+len_CD = len(CD)
+
+while i < len_AB and j < len_CD:
+    if AB[i] + CD[j] == 0:
+        count_AB, count_CD = 1, 1
+        while i + 1 < len_AB and AB[i] == AB[i + 1]:
+            count_AB += 1
+            i += 1
+        while j + 1 < len_CD and CD[j] == CD[j + 1]:
+            count_CD += 1
+            j += 1
+        ans += count_AB * count_CD
+        i += 1
+        j += 1
+    elif AB[i] + CD[j] < 0:
+        i += 1
+    else:
+        j += 1
 
 print(ans)

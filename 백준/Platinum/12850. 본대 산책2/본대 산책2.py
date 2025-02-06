acd31ce2dcb4af1697702bb342545c7b21a6747e
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-07 00:51:30 +0900

    [Platinum V] Title: 본대 산책2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Platinum/12850. 본대 산책2/본대 산책2.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+MOD = 10 ** 9 + 7
+
+def multiply(m1, m2):
+    n = len(m1)
+    m = len(m1[0])
+    p = len(m2[0])
+
+    result = [[0] * p for _ in range(n)]
+    for i in range(n):
+        for j in range(p):
+            for k in range(m):
+                result[i][j] += m1[i][k] * m2[k][j]
+                result[i][j] %= MOD
+
+    return result
+
+def power(m, n):
+    result = [[1], [0], [0], [0], [0], [0], [0], [0]]
+    while n:
+        if n % 2:
+            result = multiply(m, result)
+            n -= 1
+        m = multiply(m, m)
+        n //= 2
+    return result
+
+d = int(input())
+m = [[0, 1, 1, 0, 0, 0, 0, 0],
+     [1, 0, 1, 1, 0, 0, 0, 0],
+     [1, 1, 0, 1, 1, 0, 0, 0],
+     [0, 1, 1, 0, 1, 1, 0, 0],
+     [0, 0, 1, 1, 0, 1, 1, 0],
+     [0, 0, 0, 1, 1, 0, 0, 1],
+     [0, 0, 0, 0, 1, 0, 0, 1],
+     [0, 0, 0, 0, 0, 1, 1, 0]]
+
+ans = sum(power(m, d)[0]) % MOD
+print(ans)

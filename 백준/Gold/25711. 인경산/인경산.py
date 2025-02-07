a4c1f14627740de4be03ffb43a8e3942e0e9e690
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-07 15:30:48 +0900

    [Gold V] Title: 인경산, Time: 816 ms, Memory: 65308 KB -BaekjoonHub

---
+++ 백준/Gold/25711. 인경산/인경산.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n, q = map(int, input().split())
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+
+left = [0] * n
+right = [0] * n
+
+for i in range(1, n):
+    dx = x[i] - x[i - 1]
+    dy = y[i] - y[i - 1]
+    dist = (dx * dx + dy * dy) ** 0.5
+    if dy > 0:
+        left[i] = left[i - 1] + dist * 1
+        right[i] = right[i - 1] + dist * 3
+    elif dy == 0:
+        left[i] = left[i - 1] + dist * 2
+        right[i] = right[i - 1] + dist * 2
+    else:
+        left[i] = left[i - 1] + dist * 3
+        right[i] = right[i - 1] + dist * 1
+
+for _ in range(q):
+    i, j = map(int, input().split())
+    if i < j: ans = right[j - 1] - right[i - 1]
+    else: ans = left[i - 1] - left[j - 1]
+    print(ans)

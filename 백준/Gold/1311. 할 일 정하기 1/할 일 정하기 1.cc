de2c44ee7dbb7f387212518ab9c4367336b1d38c
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-09 15:28:52 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 64 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
@@ -3,21 +3,7 @@ using namespace std;
 
 int n;
 int cost[20][20];
-int dp[20][1 << 20];
-
-int solve(int depth, int bitmask) {
-	if (depth == n) return 0;
-	if (dp[depth][bitmask]) return dp[depth][bitmask];
-
-	int minCost = INT_MAX;
-	for (int i = 0; i < n; i++) {
-		if (bitmask & (1 << i)) continue;
-		int newCost = cost[depth][i] + solve(depth + 1, bitmask | (1 << i));
-		minCost = min(minCost, newCost);
-	}
-
-	return dp[depth][bitmask] = minCost;
-}
+vector<int> dp;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -28,8 +14,17 @@ int main() {
 		for (int j = 0; j < n; j++)
 			cin >> cost[i][j];
 
-	int ans = solve(0, 0);
-	cout << ans << '\n';
+	dp.assign(1 << n, (int)1e9);
+	dp[0] = 0;
 
+	for (int bitmask = 0; bitmask < (1 << n); bitmask++) {
+		int popCount = __builtin_popcount(bitmask);
+		for (int i = 0; i < n; i++) {
+			if (bitmask & (1 << i)) continue;
+			dp[bitmask | (1 << i)] = min(dp[bitmask | (1 << i)], dp[bitmask] + cost[popCount][i]);
+		}
+	}
+
+	cout << dp[(1 << n) - 1] << '\n';
 	return 0;
 }

1080ab2a95983c16c9b3b5da2ce1a3f53968288e
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-16 15:44:20 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 28 ms, Memory: 7056 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
@@ -3,39 +3,27 @@ using namespace std;
 
 int n, m;
 string board[1005];
-int p[1000005];
-bool vis[1005][1005];
-unordered_map<char, pair<int, int>> dir = {
-	{ 'U', { -1, 0 } },
-	{ 'D', { 1, 0 } },
-	{ 'L', { 0, -1 } },
-	{ 'R', { 0, 1 } },
-};
+int state[1005][1005];
+int ans = 0;
 
-int coordToId(int x, int y) {
-	return x * m + y;
-}
-
-int find(int x) {
-	if (p[x] == x) return x;
-	return p[x] = find(p[x]);
-}
-
-void unite(int a, int b) {
-	a = find(a);
-	b = find(b);
-	if (a != b) p[b] = a;
+pair<int, int> moveDir(char c) {
+	if (c == 'U') return { -1, 0 };
+	else if (c == 'D') return { 1, 0 };
+	else if (c == 'L') return { 0, -1 };
+	return { 0, 1 };
 }
 
 void dfs(int x, int y) {
-	vis[x][y] = true;
+	state[x][y] = 1;
 
-	auto [dx, dy] = dir[board[x][y]];
+	auto [dx, dy] = moveDir(board[x][y]);
 	int nx = x + dx;
 	int ny = y + dy;
 
-	unite(coordToId(x, y), coordToId(nx, ny));
-	if (!vis[nx][ny]) dfs(nx, ny);
+	if (state[nx][ny] == 0) dfs(nx, ny);
+	else if (state[nx][ny] == 1) ans++;
+
+	state[x][y] = 2;
 }
 
 int main() {
@@ -44,20 +32,14 @@ int main() {
 
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) cin >> board[i];
-	for (int i = 0; i < n * m; i++) p[i] = i;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (vis[i][j]) continue;
+			if (state[i][j] != 0) continue;
 			dfs(i, j);
 		}
 	}
 
-	set<int> s;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			s.insert(find(coordToId(i, j)));
-
-	cout << s.size() << '\n';
+	cout << ans << '\n';
 	return 0;
 }

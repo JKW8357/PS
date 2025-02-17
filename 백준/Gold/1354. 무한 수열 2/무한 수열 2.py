461920dbdcb73c91b82110821713c6b68516dee8
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-17 15:31:22 +0900

    [Gold V] Title: 무한 수열 2, Time: 5016 ms, Memory: 627236 KB -BaekjoonHub

--- 백준/Gold/1354. 무한 수열 2/무한 수열 2.py
+++ 백준/Gold/1354. 무한 수열 2/무한 수열 2.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 import sys
 input = sys.stdin.readline
 
@@ -6,7 +5,6 @@ n, p, q, x, y = map(int,input().split())
 dp = dict()
 dp[0] = 1
 
-@lru_cache(maxsize=None)
 def solve(i):
     if not i in dp:
         dp[i] = solve(max(i // p - x, 0)) + solve(max(i // q - y, 0))

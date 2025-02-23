b2061c3f956cd81f83830305d7b4fe618d4088a5
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-23 17:25:49 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -1,21 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[1005];
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	dp[1] = 1; 
-	dp[2] = 0;
-	dp[3] = 1;
-	dp[4] = 1;
-	dp[5] = 1;
 	int n; cin >> n;
-	for (int i = 6; i <= n; i++)
-		if (!dp[i - 1] || !dp[i - 3] || !dp[i - 4]) dp[i] = 1;
-	cout << (dp[n] ? "SK" : "CY") << '\n';
+	cout << (n % 7 != 0 && n % 7 != 2 ? "SK" : "CY") << '\n';
 
 	return 0;
 }

27f1183c85587b5a5b6c88d8cd221bfbc097d386
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-23 17:24:46 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -1,21 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[1005];
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	dp[1] = 0; 
-	dp[2] = 1;
-	dp[3] = 0;
-	dp[4] = 1;
-	dp[5] = 1;
 	int n; cin >> n;
-	for (int i = 5; i <= n; i++)
-		if (!dp[i - 1] || !dp[i - 3] || !dp[i - 4]) dp[i] = 1;
-	cout << (dp[n] ? "SK" : "CY") << '\n';
+	cout << (n % 7 != 1 && n % 7 != 3 ? "SK" : "CY") << '\n';
 
 	return 0;
 }

50cf35b92552699100bb0a72eb4f3b333974b354
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-23 17:27:39 +0900

    [Gold V] Title: 돌 게임 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long n; cin >> n;
+	cout << (n % 7 != 0 && n % 7 != 2 ? "SK" : "CY") << '\n';
+
+	return 0;
+}

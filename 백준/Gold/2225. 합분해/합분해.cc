5c6de1ce53a9e342165635be1a12a8be77053e14
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-02-27 18:53:30 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int mod = (int)1e9;
+const int MOD = (int)1e9;
 int dp[205][205];
 
 int main() {
@@ -12,10 +12,11 @@ int main() {
 	cin >> n >> k;
 
 	for (int i = 0; i <= k; i++) dp[0][i] = 1;
+	for (int i = 0; i <= n; i++) dp[i][1] = 1;
+
 	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= k; j++)
-			for (int x = 0; x <= i; x++)
-				dp[i][j] = (dp[i][j] + dp[i - x][j - 1]) % mod;
+		for (int j = 2; j <= k; j++)
+			dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
 
 	cout << dp[n][k] << '\n';
 	return 0;

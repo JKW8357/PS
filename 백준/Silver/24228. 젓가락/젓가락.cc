3d05e15a6e2c955e3bcf0e240022ebefca1d6e56
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-02 18:17:19 +0900

    [Silver V] Title: 젓가락, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long n, r;
+	cin >> n >> r;
+	cout << (n + 1) + 2 * (r - 1) << '\n';
+
+	return 0;
+}

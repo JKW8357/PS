837b6d5ee3a2fdffd837704c6c373e2c02db9126
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-05 10:40:36 +0900

    [Silver IV] Title: 초콜릿과 11과 팰린드롬, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/31460. 초콜릿과 11과 팰린드롬/초콜릿과 11과 팰린드롬.py
+++ 백준/Silver/31460. 초콜릿과 11과 팰린드롬/초콜릿과 11과 팰린드롬.py
@@ -4,6 +4,4 @@ input = sys.stdin.readline
 tc = int(input())
 for _ in range(tc):
     n = int(input())
-    if n % 4 == 1: print('1' * (n // 2) + '0' + '1' * (n // 2))
-    elif n % 4 == 3: print('1' * (n // 2) + '2' + '1' * (n // 2))
-    else: print('1' * n)
+    print('1' + '2' * (n - 2) + '1' if n > 1 else 0)

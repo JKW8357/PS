496d09ccdea9508d85ebb75c4918c97f384fbaa0
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-19 20:11:57 +0900

    [Silver IV] Title: 수 찾기, Time: 84 ms, Memory: 6776 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[100005];
+set<int> s;
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) cin >> arr[i];
-	sort(arr, arr + n);
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num; cin >> num;
+		s.insert(num);
+	}
 
-	int m;
-	cin >> m;
-	while(m--) {
-		int q;
-		cin >> q;
-		cout << (binary_search(arr, arr + n, q) ? 1 : 0) << '\n';
+	int m; cin >> m;
+	while (m--) {
+		int num; cin >> num;
+		cout << (s.count(num) ? 1 : 0) << '\n';
 	}
 
 	return 0;

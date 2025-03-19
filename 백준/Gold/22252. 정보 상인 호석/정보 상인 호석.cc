b1ad13e861ecc3ae574e061c3b0834a4d01c05af
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-19 17:25:13 +0900

    [Gold V] Title: 정보 상인 호석, Time: 348 ms, Memory: 3884 KB -BaekjoonHub

---
+++ 백준/Gold/22252. 정보 상인 호석/정보 상인 호석.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<string, priority_queue<int>> m;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	long long ans = 0;
+	int q; cin >> q;
+	while (q--) {
+		int query; cin >> query;
+		if (query == 1) {
+			string name; cin >> name;
+			int k; cin >> k;
+			while (k--) {
+				int c; cin >> c;
+				m[name].push(c);
+			}
+		}
+
+
+		else {
+			string name; cin >> name;
+			int b; cin >> b;
+			while (!m[name].empty() && b--) {
+				ans += m[name].top();
+				m[name].pop();
+			}
+		}
+	}
+
+	cout << ans << '\n';
+	return 0;
+}

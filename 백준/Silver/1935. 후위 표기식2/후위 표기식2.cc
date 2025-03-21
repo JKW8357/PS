43e3266b292b4bf9fa0ab5d46b30ca99f2d4b4e8
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-21 09:30:25 +0900

    [Silver III] Title: 후위 표기식2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.cc
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.cc
@@ -1,34 +1,93 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+typedef double Data;
 
-double number[26];
-stack<double> st;
+class ArrayStack {
+private:
+	Data* elements;
+	int capacity;
+	int top_idx;
+
+public:
+	ArrayStack(int N) : elements(new Data[N]), capacity(N), top_idx(-1) {}
+	~ArrayStack() { delete[] elements; }
+	int size() { return top_idx + 1; }
+	bool empty() { return size() == 0; }
+	void push(Data val) {
+		if (size() >= capacity) return;
+		elements[++top_idx] = val;
+	}
+	void pop() {
+		if (empty()) return;
+		--top_idx;
+	}
+	int top() { return !empty() ? elements[top_idx] : -1; }
+};
+
+
+
+struct Node {
+	Data dat;
+	Node* nxt;
+};
+
+class ListStack {
+private:
+	Node* topNode;
+	int siz;
+
+public:
+	ListStack() : topNode(nullptr), siz(0) {}
+	~ListStack() {
+		Node* cur = topNode;
+		while (cur != nullptr) {
+			Node* tmpNode = cur;
+			cur = cur->nxt;
+			delete tmpNode;
+		}
+	}
+	int size() { return siz; }
+	bool empty() { return siz == 0; }
+	void push(Data val) {
+		Node* newNode = new Node{ val, topNode };
+		topNode = newNode;
+		siz++;
+	}
+	void pop() {
+		if (empty()) return;
+		Node* delNode = topNode;
+		topNode = topNode->nxt;
+		delete delNode;
+		siz--;
+	}
+	Data top() { return !empty() ? topNode->dat : -1; }
+};
+
+Data mapping[26];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	int n; cin >> n;
+	string postfix; cin >> postfix;
+	for (int i = 0; i < n; i++) cin >> mapping[i];
 
-	int n;
-	string postfix;
-	cin >> n >> postfix;
-	for (int i = 0; i < n; i++) cin >> number[i];
-
+	ListStack st;
 	for (char c : postfix) {
-		if (isalpha(c)) st.push(number[c - 'A']);
+		if (c - 'A' >= 0 && c - 'A' < 26) st.push(mapping[c - 'A']);
 		else {
-			double num2 = st.top(); st.pop();
-			double num1 = st.top(); st.pop();
-			
-			if (c == '+') st.push(num1 + num2);
-			else if (c == '-') st.push(num1 - num2);
-			else if (c == '*') st.push(num1 * num2);
-			else st.push(num1 / num2);
+			double second = st.top(); st.pop();
+			double first = st.top(); st.pop();
+
+			double result = 0;
+			if (c == '+') result = first + second;
+			if (c == '-') result = first - second;
+			if (c == '*') result = first * second;
+			if (c == '/') result = first / second;
+
+			st.push(result);
 		}
 	}
 
-	double result = st.top();
-	cout << fixed; cout.precision(2);
-	cout << result << '\n';
-
-	return 0;
+	double ans = st.top(); st.pop();
+	cout.precision(2);
+	cout << fixed << ans << '\n';
 }

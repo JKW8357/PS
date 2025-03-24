12805aa871736e0e02bbe658f4da1b67869cf697
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-24 22:51:29 +0900

    [Silver II] Title: 아카라카, Time: 32 ms, Memory: 8008 KB -BaekjoonHub

--- 백준/Silver/23304. 아카라카/아카라카.cc
+++ 백준/Silver/23304. 아카라카/아카라카.cc
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool akaraka(string& str) {
-	int size = str.size();
-	if (size == 1) return true;
+bool isAkaraka(string& s) {
+	if (s.size() == 1) return true;
 
-	string tmp(str);
-	reverse(tmp.begin(), tmp.end());
-	if (str != tmp) return false;
+	string left = s.substr(0, s.size() / 2);
+	string right = s.substr((s.size() + 1) / 2);
 
-	string s1 = str.substr(0, size / 2);
-	string s2 = str.substr(size - size / 2, size);
-
-	return (akaraka(s1) && akaraka(s2));
+	return left == right && isAkaraka(left) && isAkaraka(right);
 }
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-	string str;
-	cin >> str;
-	cout << (akaraka(str) ? "AKARAKA\n" : "IPSELENTI\n");
+	string s; cin >> s;
+	cout << (isAkaraka(s) ? "AKARAKA\n" : "IPSELENTI\n");
 
 	return 0;
 }

708af08648717e499fafe99af6b777f996ada393
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-24 22:03:27 +0900

    [Silver II] Title: 영재의 시험, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.cc
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.cc
@@ -2,27 +2,28 @@
 using namespace std;
 
 int res = 0;
-int answer[12];
+int answer[10];
 
-void solve(int score, int num, int choosed1, int choosed2) {
-	if (num == 10) {
+void solve(int questionNum, int score, int prev, int prevPrev) {
+	if (questionNum == 10) {
 		if (score >= 5) res++;
 		return;
 	}
 
 	for (int i = 1; i <= 5; i++) {
-		if (choosed1 == choosed2 && choosed2 == i) continue;
-		if (answer[num] == i) solve(score + 1, num + 1, choosed2, i);
-		else solve(score, num + 1, choosed2, i);
+		if (i == prev && prev == prevPrev) continue;
+		if (answer[questionNum] == i) solve(questionNum + 1, score + 1, i, prev);
+		else solve(questionNum + 1, score, i, prev);
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
 	for (int i = 0; i < 10; i++) cin >> answer[i];
-	solve(0, 0, 0, 0);
+	solve(0, 0, -1, -1);
 	cout << res << '\n';
 
 	return 0;

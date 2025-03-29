34553a691a7cd48ce6c1735a336f5227f445e6d3
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-29 23:27:20 +0900

    [Gold II] Title: 플로이드 2, Time: 28 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = (int)1e9;
+vector<vector<int>> adj, nxt;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+	adj.assign(n + 1, vector<int>(n + 1, INF));
+	nxt.assign(n + 1, vector<int>(n + 1, -1));
+	for (int i = 1; i <= n; i++) adj[i][i] = 0;
+
+	while (m--) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		if (adj[a][b] > c) {
+			adj[a][b] = c;
+			nxt[a][b] = b;
+		}
+	}
+
+	for (int k = 1; k <= n; k++)
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				if (adj[i][j] > adj[i][k] + adj[k][j]) {
+					adj[i][j] = adj[i][k] + adj[k][j];
+					nxt[i][j] = nxt[i][k];
+				}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++)
+			cout << (adj[i][j] != INF ? adj[i][j] : 0) << ' ';
+		cout << '\n';
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (adj[i][j] == INF || adj[i][j] == 0) {
+				cout << 0 << '\n';
+				continue;
+			}
+			vector<int> path;
+			int cur = i;
+			while (cur != j) {
+				path.push_back(cur);
+				cur = nxt[cur][j];
+			}
+			path.push_back(cur);
+			cout << path.size() << ' ';
+			for (int x : path) cout << x << ' ';
+			cout << '\n';
+		}
+	}
+
+	return 0;
+}

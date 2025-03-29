6da92b3c5cae0e9a5c7980bbc371bece4dad105e
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-30 01:32:37 +0900

    [Gold IV] Title: 저울, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+const int INF = (int)1e9;
+vector<vector<int>> adj;
+
+void floyd() {
+	for (int k = 1; k <= n; k++)
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				if (adj[i][k] == 1 && adj[k][j] == 1) adj[i][j] = 1;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n >> m;
+	adj.assign(n + 1, vector<int>(n + 1, INF));
+	for (int i = 1; i <= n; i++) adj[i][i] = 0;
+
+	while (m--) {
+		int a, b;
+		cin >> a >> b;
+		adj[a][b] = 1;
+	}
+
+	floyd();
+
+	for (int i = 1; i <= n; i++) {
+		int cnt = n - 1;
+		for (int j = 1; j <= n; j++)
+			if (adj[i][j] == 1 || adj[j][i] == 1) cnt--;
+		cout << cnt << '\n';
+	}
+
+	return 0;
+}

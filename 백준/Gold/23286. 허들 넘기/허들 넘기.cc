e092d3006bd2174c2077ba1e6194ee5cb2fd0f05
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-30 04:10:18 +0900

    [Gold III] Title: 허들 넘기, Time: 52 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/23286. 허들 넘기/허들 넘기.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, tc;
+const int INF = (int)1e9;
+vector<vector<int>> adj;
+
+void floyd() {
+	for (int k = 1; k <= n; k++)
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				if (adj[i][j] > adj[i][k] && adj[i][j] > adj[k][j])
+					adj[i][j] = max(adj[i][k], adj[k][j]);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n >> m >> tc;
+	adj.assign(n + 1, vector<int>(n + 1, INF));
+
+	while (m--) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		adj[a][b] = c;
+	}
+
+	floyd();
+
+	while (tc--) {
+		int s, e;
+		cin >> s >> e;
+		cout << (adj[s][e] != INF ? adj[s][e] : -1) << '\n';
+	}
+
+	return 0;
+}

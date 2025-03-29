86c2545757220d95bae5138e97f2b92c826265fc
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-30 06:25:58 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -4,26 +4,25 @@ typedef pair<int, int> pii;
 
 int n, m, x;
 const int INF = (int)1e9;
-vector<vector<pii>> adj;
-vector<int> dist;
+vector<vector<pii>> adj, revAdj;
 
-int dijkstra(int st, int en) {
+vector<int> dijkstra(int st, vector<vector<pii>>& graph) {
 	priority_queue<pii, vector<pii>, greater<pii>> pq;
-	dist.assign(n + 1, INF);
+	vector<int> dist(n + 1, INF);
 	dist[st] = 0;
 	pq.push({ dist[st], st });
 
 	while (!pq.empty()) {
 		auto [c, cur] = pq.top(); pq.pop();
 		if (dist[cur] != c) continue;
-		for (auto [nc, nxt] : adj[cur]) {
+		for (auto [nc, nxt] : graph[cur]) {
 			if (dist[nxt] <= dist[cur] + nc) continue;
 			dist[nxt] = dist[cur] + nc;
 			pq.push({ dist[nxt], nxt });
 		}
 	}
 
-	return dist[en];
+	return dist;
 }
 
 int main() {
@@ -32,15 +31,20 @@ int main() {
 
 	cin >> n >> m >> x;
 	adj.assign(n + 1, vector<pii>());
+	revAdj.assign(n + 1, vector<pii>());
 	while (m--) {
 		int a, b, c;
 		cin >> a >> b >> c;
 		adj[a].push_back({ c, b });
+		revAdj[b].push_back({ c, a });
 	}
+
+	auto v1 = dijkstra(x, adj);
+	auto v2 = dijkstra(x, revAdj);
 
 	int maxTime = 0;
 	for (int i = 1; i <= n; i++) {
-		int d = dijkstra(i, x) + dijkstra(x, i);
+		int d = v1[i] + v2[i];
 		maxTime = max(maxTime, d);
 	}
 	

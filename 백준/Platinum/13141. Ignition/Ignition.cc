b8568c2653316b5b97c86ccbdde8b1f1b5d30dca
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-31 22:33:36 +0900

    [Platinum V] Title: Ignition, Time: 52 ms, Memory: 3744 KB -BaekjoonHub

---
+++ 백준/Platinum/13141. Ignition/Ignition.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+const int INF = (int)1e9;
+vector<vector<int>> adj;
+vector<vector<vector<int>>> edges;
+
+void floyd() {
+	for (int k = 1; k <= n; k++)
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n >> m;
+	adj.assign(n + 1, vector<int>(n + 1, INF));
+	edges.assign(n + 1, vector(n + 1, vector<int>()));
+	for (int i = 1; i <= n; i++) adj[i][i] = 0;
+
+	for (int i = 1; i <= m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		adj[a][b] = min(adj[a][b], c);
+		adj[b][a] = min(adj[b][a], c);
+		edges[a][b].push_back(c);
+	}
+
+	floyd();
+
+	double minTime = (double)INF;
+	for (int st = 1; st <= n; st++) {
+		double totalTime = 0;
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				for (int cost : edges[i][j])
+					totalTime = max(totalTime, (double)(adj[st][i] + adj[st][j] + cost) / 2);
+		minTime = min(minTime, totalTime);
+	}
+
+	cout << fixed << setprecision(1) << minTime << '\n';
+	return 0;
+}

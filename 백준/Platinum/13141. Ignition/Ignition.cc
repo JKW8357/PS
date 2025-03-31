b309e429d51f998181bac18b19c61384c310e520
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-03-31 22:38:12 +0900

    [Platinum V] Title: Ignition, Time: 20 ms, Memory: 2896 KB -BaekjoonHub

--- 백준/Platinum/13141. Ignition/Ignition.cc
+++ 백준/Platinum/13141. Ignition/Ignition.cc
@@ -4,7 +4,7 @@ using namespace std;
 int n, m;
 const int INF = (int)1e9;
 vector<vector<int>> adj;
-vector<vector<vector<int>>> edges;
+vector<tuple<int, int, int>> edges;
 
 void floyd() {
 	for (int k = 1; k <= n; k++)
@@ -20,7 +20,6 @@ int main() {
 
 	cin >> n >> m;
 	adj.assign(n + 1, vector<int>(n + 1, INF));
-	edges.assign(n + 1, vector(n + 1, vector<int>()));
 	for (int i = 1; i <= n; i++) adj[i][i] = 0;
 
 	for (int i = 1; i <= m; i++) {
@@ -28,7 +27,7 @@ int main() {
 		cin >> a >> b >> c;
 		adj[a][b] = min(adj[a][b], c);
 		adj[b][a] = min(adj[b][a], c);
-		edges[a][b].push_back(c);
+		edges.push_back({ a, b, c });
 	}
 
 	floyd();
@@ -36,10 +35,8 @@ int main() {
 	double minTime = (double)INF;
 	for (int st = 1; st <= n; st++) {
 		double totalTime = 0;
-		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= n; j++)
-				for (int cost : edges[i][j])
-					totalTime = max(totalTime, (double)(adj[st][i] + adj[st][j] + cost) / 2);
+		for (auto& [u, v, c] : edges)
+			totalTime = max(totalTime, (double)(adj[st][u] + adj[st][v] + c) / 2);
 		minTime = min(minTime, totalTime);
 	}
 

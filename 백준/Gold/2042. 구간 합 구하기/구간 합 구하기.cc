4f048f2c7579bdf83f5f8abc59b88cae4f15c074
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-04-01 01:04:11 +0900

    [Gold I] Title: 구간 합 구하기, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int n, m, k;
+vector<ll> fenwickTree;
+vector<ll> arr;
+
+ll sum(int idx) {
+	ll result = 0;
+	while (idx) {
+		result += fenwickTree[idx];
+		idx -= (idx & -idx);
+	}
+	return result;
+}
+
+void update(int idx, ll diff) {
+	while (idx <= (int)fenwickTree.size()) {
+		fenwickTree[idx] += diff;
+		idx += (idx & -idx);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n >> m >> k;
+	fenwickTree.assign(n + 1, 0);
+	arr.assign(n + 1, 0);
+	for (int i = 1; i <= n; i++) {
+		ll x; cin >> x;
+		arr[i] = x;
+		update(i, x);
+	}
+
+	int q = m + k;
+	while (q--) {
+		int query; cin >> query;
+		if (query == 1) {
+			int idx; ll val;
+			cin >> idx >> val;
+			ll diff = val - arr[idx];
+			arr[idx] = val;
+			update(idx, diff);
+		}
+		else {
+			int st, en;
+			cin >> st >> en;
+			cout << sum(en) - sum(st - 1) << '\n';
+		}
+	}
+
+	return 0;
+}

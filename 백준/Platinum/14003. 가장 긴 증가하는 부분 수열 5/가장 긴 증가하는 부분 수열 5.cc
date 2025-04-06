a3762c7785afb111c149ba5bf6527a8e11d6a806
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-04-07 03:52:35 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 196 ms, Memory: 22252 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -13,7 +13,6 @@ vector<int> LIS(const vector<int>& vec) {
 	}
 
 	int len = (int)tmp.size();
-	cout << len << '\n';
 	for (int i = size - 1; i >= 0; i--) {
 		if (idx[i] == len - 1) {
 			result.push_back(vec[i]);
@@ -34,6 +33,7 @@ int main() {
 	for (int i = 0; i < n; i++) cin >> vec[i];
 
 	vector<int> lis = LIS(vec);
+	cout << lis.size() << '\n';
 	for (int x : lis) cout << x << ' ';
 
 	return 0;

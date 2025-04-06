a417026e4ed5a3fff0154055dacc2e6c92747ea2
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-04-06 18:16:42 +0900

    [Silver IV] Title: 동가수열 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/25184. 동가수열 구하기/동가수열 구하기.cc
+++ 백준/Silver/25184. 동가수열 구하기/동가수열 구하기.cc
@@ -1,29 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> result;
-vector<bool> used(5005);
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
 	int n; cin >> n;
-	if (n == 1) {
-		cout << 1 << '\n';
-		return 0;
-	}
+	for (int i = n / 2; i >= 1; i--)
+		cout << i << ' ' << i + n / 2 << ' ';
+	if (n % 2) cout << n << '\n';
 
-	for (int st = (n + 1) / 2; st >= 1; st--) {
-		for (int i = st; i <= n; i += n / 2) {
-			if (!used[i]) {
-				used[i] = true;
-				result.push_back(i);
-			}
-		}
-	}
-
-	for (int x : result) cout << x << ' ';
 	return 0;
 }

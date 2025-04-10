73eb6e03e69c8b746802698cfa5bf8c0b8f058a8
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-04-10 12:46:44 +0900

    [Silver III] Title: 숫자 POP, Time: 184 ms, Memory: 110744 KB -BaekjoonHub

--- 백준/Silver/32713. 숫자 POP/숫자 POP.py
+++ 백준/Silver/32713. 숫자 POP/숫자 POP.py
@@ -1,23 +1,18 @@
-from itertools import groupby
 import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-segments = [[key, sum(1 for _ in group)] for key, group in groupby(arr)]
-
 ans = 0
-for i in range(len(segments)):
-    num = segments[i][0]
+for i in range(n):
+    num = arr[i]
     cnt = deleted = 0
 
-    for j in range(i, len(segments)):
-        if segments[j][0] != num:
-            deleted += segments[j][1]
-            if deleted > k: break
-        else:
-            cnt += segments[j][1]
+    for j in range(i, n):
+        if arr[j] == num: cnt += 1
+        else: deleted += 1
+        if deleted > k: break
         ans = max(ans, cnt)
 
 print(ans)

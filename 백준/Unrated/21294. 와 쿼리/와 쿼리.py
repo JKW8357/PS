48c987918e7fc838a1f722456d9f70b94da7ef2b
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-04-21 06:15:49 +0900

    [Unrated] Title: 와 쿼리, Time: 404 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Unrated/21294. 와 쿼리/와 쿼리.py
+++ 백준/Unrated/21294. 와 쿼리/와 쿼리.py
@@ -2,8 +2,4 @@ import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    query = input().strip()
-    if query == '1':
-        pass
-    if query == '2':
-        print()
+    if input().strip() == '2': print()

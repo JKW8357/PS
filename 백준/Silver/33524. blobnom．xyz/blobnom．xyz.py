a2b18f5fc8ae1875587740806ab977172bc3cf67
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-05-11 16:09:38 +0900

    [Silver III] Title: blobnom.xyz, Time: 444 ms, Memory: 183352 KB -BaekjoonHub

--- 백준/Silver/33524. blobnom．xyz/blobnom．xyz.py
+++ 백준/Silver/33524. blobnom．xyz/blobnom．xyz.py
@@ -5,11 +5,11 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 problems = sorted(map(int, input().split()))
 level = list(map(int, input().split()))
-size = [0] * len(level)
+size = []
 threshold = [3 * i ** 2 - 3 * i + 1 for i in range(1, 580)]
 
-for i in range(len(size)):
+for i in range(len(level)):
     can_solve = bisect_right(problems, level[i])
-    size[i] = bisect_right(threshold, can_solve)
+    size.append(bisect_right(threshold, can_solve))
 
 print(*size)

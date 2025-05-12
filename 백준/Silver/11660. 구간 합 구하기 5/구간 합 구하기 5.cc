13f65c5a5dbf93fcb45170650f51e9fa5a74dbb8
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-05-12 16:22:41 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 116 ms, Memory: 10308 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -2,29 +2,27 @@
 using namespace std;
 
 int arr[1030][1030];
-int dp[1030][1030];
+int prefix[1030][1030];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	int n, m;
-	cin >> n >> m;
-	
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cin >> arr[i][j];
-			dp[i][j] = arr[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
-		}
-	}
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> arr[i][j];
+            prefix[i][j] = arr[i][j] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
+        }
+    }
 
-	while (m--) {
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
+    while (m--) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << prefix[x2][y2] - prefix[x2][y1 - 1] - prefix[x1 - 1][y2] + prefix[x1 - 1][y1 - 1] << '\n';
+    }
 
-		int ans = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
-		cout << ans << '\n';
-	}
-
-	return 0;
+    return 0;
 }

8b5c6d1ad5ec92bf31ffd44758c3865cc4c203c4
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-05-15 21:34:23 +0900

    [Gold IV] Title: 팬케이크맛 쿠키, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/24728. 팬케이크맛 쿠키/팬케이크맛 쿠키.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include "pancakecookie.h"
+using namespace std;
+
+PanCakeCookie::PanCakeCookie(int w, int h, int maxC, int u, int d)
+    : w(w), h(h), x(0), y(0), t(0), score(0), maxC(maxC), curC(maxC), u(u), d(d) {
+}
+
+int PanCakeCookie::getX() { return this->x; }
+int PanCakeCookie::getY() { return this->y; }
+int PanCakeCookie::getU() { return this->u; }
+int PanCakeCookie::getD() { return this->d; }
+int PanCakeCookie::getScore() { return this->score; }
+int PanCakeCookie::getC() { return this->curC; }
+
+void PanCakeCookie::reset() {
+    this->x = this->y = this->t = this->score = 0;
+    this->curC = this->maxC;
+}
+
+void PanCakeCookie::setU(int x) {
+    if (x < 1 || x > 1000) return;
+    this->u = x;
+    reset();
+}
+
+void PanCakeCookie::setD(int x) {
+    if (x < 1 || x > 1000) return;
+    this->d = x;
+    reset();
+}
+
+void PanCakeCookie::setC(int x) {
+    if (x < 1 || x > 1000) return;
+    this->maxC = x;
+    reset();
+}
+
+void PanCakeCookie::setT(int t) {
+    if (t < 0 || t >= this->w) return;
+    reset();
+
+    this->x = this->t = t;
+
+    for (int i = 1; i <= t; ++i) {
+        if (curC > 0) {
+            y = min(h, y + u);
+            curC--;
+        } 
+        else {
+            y = max(0, y - d);
+            if (curC < maxC) curC++;
+        }
+        score += y;
+    }
+}

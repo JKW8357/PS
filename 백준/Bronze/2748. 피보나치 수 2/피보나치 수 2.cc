b8fb14da4a5c50d3871b1f4d2b32a8909fa5f794
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-05-21 18:04:38 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -4,25 +4,20 @@ typedef long long ll;
 
 ll dp[100];
 
-ll fibonacci(int n) {
-    // base case
-    if (n == 0 || n == 1) return n;
-
-    // 이전에 계산한 값 재활용
-    if (dp[n] != -1) return dp[n];
-
-    // recursive case
-    return dp[n] = fibonacci(n - 1) + fibonacci(n - 2);
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
     int n; cin >> n;
-    fill(dp, dp + n + 1, -1);
-    cout << fibonacci(n) << '\n';
 
+    // base case
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
+    cout << dp[n] << '\n';
     return 0;
 }

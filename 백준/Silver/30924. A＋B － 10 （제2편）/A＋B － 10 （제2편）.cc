d11ac1cd4a4e5b3c704be3c28af24b65a16a8656
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-05-30 17:47:18 +0900

    [Silver V] Title: A+B - 10 (제2편), Time: 772 ms, Memory: 13676 KB -BaekjoonHub

--- 백준/Silver/30924. A＋B － 10 （제2편）/A＋B － 10 （제2편）.cc
+++ 백준/Silver/30924. A＋B － 10 （제2편）/A＋B － 10 （제2편）.cc
@@ -1,54 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-unordered_set<int> A, B;
+int findNum(char c) {
+    set<int> s;
+    int num, ans, res;
+    bool found = false;
+    for (int i = 1; i <= 10000; i++) s.insert(i);
+    for (int i = 1; i <= 9999; i++) {
+        num = rand() % 10000 + 1;
+        while (!s.count(num)) num = rand() % 10000 + 1;
+        cout << "? " << c << ' ' << num << endl;
+        cin >> ans;
+        if (s.size() >= 2) s.erase(num);
+        if (ans) {
+            res = num;
+            found = true;
+            break;
+        }
+    }
+    if (!found) res = *s.begin();
+    return res;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
-    srand((unsigned int)time(NULL));
 
-    int a, b, ans, num;
-    bool found = false;
-
-    for (int i = 1; i <= 10000; i++) {
-        A.insert(i);
-        B.insert(i);
-    }
-
-    for (int i = 1; i <= 9999; i++) {
-        num = rand() % 10000 + 1;
-        while (!A.count(num)) num = rand() % 10000 + 1;
-        cout << "? A " << num << endl;
-        cin >> ans;
-        if (A.size() >= 2) A.erase(num);
-        if (ans) { 
-            a = num;
-            found = true;
-            break;
-        }
-    }
-
-    if (!found) a = *A.begin();
-    found = false;
-
-    for (int i = 1; i <= 9999; i++) {
-        num = rand() % 10000 + 1;
-        while (!B.count(num)) num = rand() % 10000 + 1;
-        cout << "? B " << num << endl;
-        cin >> ans;
-        if (B.size() >= 2) B.erase(num);
-        if (ans) {
-            b = num;
-            found = true;
-            break;
-        }
-    }
-
-    if (!found) b = *B.begin();
-    
+    srand((unsigned int)time(0));
+    int a = findNum('A');
+    int b = findNum('B');
     cout << "! " << a + b << endl;
     return 0;
 }

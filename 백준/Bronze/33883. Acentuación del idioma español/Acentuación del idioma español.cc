e85f4c6dd0d0e048ab0e3210c3a9fda782d3be34
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-05-31 22:11:46 +0900

    [Bronze III] Title: Acentuación del idioma español, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/33883. Acentuación del idioma español/Acentuación del idioma español.cc
+++ 백준/Bronze/33883. Acentuación del idioma español/Acentuación del idioma español.cc
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-string vns = "aeiouns", v = "aeiou";
+string o="aeiouns",v="aeiou";
 int main() {
-    string s; cin >> s; int idx = -1;
-    int vc = count_if(s.begin(), s.end(), [&](char c) { return string(v).find(c) != -1; });
-    if (vc >= 2 && vns.find(s.back()) != -1) idx = s.find_last_of(v, s.find_last_of(v) - 1);
-    else if (vns.find(s.back()) == -1) idx = s.find_last_of(v);
-    cout << (idx != -1 ? idx + 1 : -1);
+    string s;cin >> s;int i=-1;
+    int vc=count_if(s.begin(),s.end(),[&](char c){return string(v).find(c)!=-1;});
+    if(vc>=2&&o.find(s.back())!=-1)i=s.find_last_of(v,s.find_last_of(v)-1);
+    if(o.find(s.back())==-1)i=s.find_last_of(v);
+    cout<<(i!=-1?i+1:-1);
 }

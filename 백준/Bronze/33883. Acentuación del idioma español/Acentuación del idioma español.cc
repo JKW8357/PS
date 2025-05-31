2932056e05f3f3cbdf55e87d7223004627e26ac8
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-05-31 22:05:28 +0900

    [Bronze III] Title: Acentuación del idioma español, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/33883. Acentuación del idioma español/Acentuación del idioma español.cc
+++ 백준/Bronze/33883. Acentuación del idioma español/Acentuación del idioma español.cc
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 const size_t npos = string::npos;
-
+string vns = "aeiouns", v = "aeiou";
 int main() {
     string s; cin >> s; int idx = npos;
-    int vc = count_if(s.begin(), s.end(), [&](char c) { return string("aeiou").find(c) != npos; });
-    if (vc >= 2 && string("aeiouns").find(s.back()) != npos) idx = s.find_last_of("aeiou", s.find_last_of("aeiou") - 1);
-    else if (string("aeiouns").find(s.back()) == npos) idx = s.find_last_of("aeiou");
-    cout << (idx != npos ? idx + 1 : -1) << '\n';
+    int vc = count_if(s.begin(), s.end(), [&](char c) { return string(v).find(c) != npos; });
+    if (vc >= 2 && vns.find(s.back()) != npos) idx = s.find_last_of(v, s.find_last_of(v) - 1);
+    else if (vns.find(s.back()) == npos) idx = s.find_last_of(v);
+    cout << (idx != npos ? idx + 1 : -1);
 }

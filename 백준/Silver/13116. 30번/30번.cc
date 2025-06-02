ada4b7e806da15937b320584281eb42508fbcb20
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-06-02 12:09:39 +0900

    [Silver IV] Title: 30번, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13116. 30번/30번.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tc; cin >> tc;
+    while (tc--) {
+        int a, b;
+        cin >> a >> b;
+        if (a > b) swap(a, b);
+        while (a != b) {
+            if (a > b) a /= 2;
+            else b /= 2;
+        }
+        cout << 10 * a << '\n';
+    }
+
+    return 0;
+}

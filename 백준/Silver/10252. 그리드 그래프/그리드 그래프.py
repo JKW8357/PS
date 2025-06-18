dd7772adeb01ffb29d6ac40b34d688d35dba1398
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-06-18 14:36:50 +0900

    [Silver II] Title: 그리드 그래프, Time: 212 ms, Memory: 113332 KB -BaekjoonHub

---
+++ 백준/Silver/10252. 그리드 그래프/그리드 그래프.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    m, n = map(int, input().split())
+    print(1)
+    for j in range(n):
+        if j % 2:
+            print(*(f'({i},{j})' for i in range(m - 2, -1, -1)), sep='\n')
+        else:
+            print(*(f'({i},{j})' for i in range(m - 1)), sep='\n')
+    print(*(f'({m - 1},{j})' for j in range(n - 1, -1, -1)), sep='\n')

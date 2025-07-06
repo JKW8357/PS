00909cae8adaa0d4b1dea29c1e3099f77cc0eae3
JKW8357 <164450033+JKW8357@users.noreply.github.com>
2025-07-06 15:32:11 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -1,12 +1,3 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	unsigned long long x; cin >> x;
-	cout << popcount(x) << '\n';
-	return 0;
-}
+int main(){unsigned long long x;cin>>x;cout<<popcount(x);}
